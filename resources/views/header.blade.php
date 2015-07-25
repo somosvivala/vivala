4ac96a00969294b3ead01560bf097e5646eddfbc
Evandro Carreira <evandro.carreira@gmail.com>
2015-07-24 22:35:23 -0300

    Layout Novo: Menu do topo

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,36 +1,35 @@
-<div class="col-sm-1 menu-esquerda">
+<div class="col-sm-2 menu-esquerda">
 	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 		<span class="sr-only">Toggle Navigation</span>
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
 	</button>
-	<a class="navbar-brand" href="{{ url('home') }}">Vivalá</a>
+	<a class="navbar-brand logo" href="{{ url('home') }}"><img src="/logo.png" alt="Vivalá" title="Vivalá"></a>
 </div>
 
-<div class="col-sm-9 menu-principal">
-	<ul class="nav navbar-nav">
+<div class="col-sm-8 menu-principal">
+	<div class="col-sm-5">
+		<input class="busca-geral-menu" placeholder="Digite aqui sua busca" type="search">
+	</div>
+	<ul class="col-sm-7 nav navbar-nav lista-intervalo-preto">
 		@if (Auth::guest())
-			<li><a href="{{ url('/auth/login') }}">{{ trans("menu.login") }}</a></li>
+			<li class="col-sm-4"><a href="{{ url('/auth/login') }}">{{ trans("menu.login") }}</a></li>
 			<li><a href="{{ url('/auth/register') }}">{{ trans("menu.signup") }}</a></li>
 		@else
-			<li><a href="{{ url('perfil') }}">{{ trans("menu.profile") }}</a></li>
-			<li><a href="{{ url('/viajar') }}">{{ trans("menu.trips") }}</a></li>
-			<li><a href="{{ url('/conectar') }}">{{ trans("menu.social") }}</a></li>
-			<li><a href="{{ url('/cuidar') }}">{{ trans("menu.ongs") }}</a></li>
-			<li><a href="{{ url('/auth/login') }}">{{ trans("menu.more") }}</a></li>
-			<li><a href="{{ url('/auth/login') }}">{{ trans("menu.contact") }}</a></li>
-			<li><a href="{{ url('/auth/login') }}">{{ trans("menu.search") }}</a></li>
+			<li class="col-sm-4"><a href="{{ url('/viajar') }}">{{ trans("menu.trips") }}</a></li>
+			<li class="col-sm-4"><a href="{{ url('/conectar') }}">{{ trans("menu.social") }}</a></li>
+			<li class="col-sm-4"><a href="{{ url('/cuidar') }}">{{ trans("menu.ongs") }}</a></li>
 		@endif
 	</ul>
 </div>
 
-<div class="col-sm-2 menu-direita">
+<div class="col-sm-2 menu-usuario">
 	<ul class="nav navbar-nav ">
 		@if (Auth::user())
 			<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-					{{ trans("menu.hello") }} {{ Auth::user()->username }}
+					<div class="pull-left hello">{{ trans("menu.hello") }} {{ Auth::user()->username }}</div>
 					<img src="{{ Auth::user()->perfil->getAvatarUrl() }}" alt="{{ Auth::user()->username }}">
 					<ul class="dropdown-menu" role="menu">
 						<li><a href="{{ url('perfil') }}">{{ trans("menu.profile") }}</a></li>
