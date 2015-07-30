8941d373e0a0226f0b5bab5ca61a3450cd7fdcac
Evandro Carreira <evandro.carreira@gmail.com>
2015-07-29 21:49:37 -0300

    Front end barra do post

--- resources/views/post/_barrainfos.blade.php
+++ resources/views/post/_barrainfos.blade.php
@@ -1,12 +1,33 @@
-<div class="col-sm-12 " id="barra-post-{{ $Post->id }}">
-	<a href="#perfilcontroller/likepost/{{ $Post->id }}" class="like-btn"><i class="fa fa-heart {{ $Post->likedByMe() }}"></i></a>
-	<span class="qtd-likes">
-		@if($Post->getQuantidadeLikes() > 1)
-			 {{ $Post->getQuantidadeLikes() }} Curtidas
-		@elseif($Post->getQuantidadeLikes() == 1)
-			 {{ $Post->getQuantidadeLikes() }} Curtida
-		@else
-			Curtir
-		@endif
-	</span>
+<div class="row barra-post" id="barra-post-{{ $Post->id }}">
+
+	<div class="tag col-sm-2">
+		<a href="#perfilcontroller/likepost/{{ $Post->id }}" class="like-btn"><i class="fa fa-tag {{ $Post->likedByMe() }}"></i></a>
+		<span class="qtd-likes">
+			
+		</span>
+	</div>
+	<div class="like col-sm-2">
+		<a href="#perfilcontroller/likepost/{{ $Post->id }}" class="like-btn"><i class="fa fa-heart {{ $Post->likedByMe() }}"></i></a>
+		<span class="qtd-likes">
+			@if($Post->getQuantidadeLikes() > 1)
+				 {{ $Post->getQuantidadeLikes() }} Curtidas
+			@elseif($Post->getQuantidadeLikes() == 1)
+				 {{ $Post->getQuantidadeLikes() }} Curtida
+			@else
+				Curtir
+			@endif
+		</span>
+	</div>
+	<div class="comentario col-sm-2">
+		<a href="#perfilcontroller/likepost/{{ $Post->id }}" class="like-btn"><i class="fa fa-comment {{ $Post->likedByMe() }}"></i></a>
+		<span class="qtd-likes">
+			0 Comentários
+		</span>
+	</div>
+	<div class="share col-sm-2">
+		<a href="#perfilcontroller/likepost/{{ $Post->id }}" class="like-btn"><i class="fa fa-share {{ $Post->likedByMe() }}"></i></a>
+		<span class="qtd-likes">
+			Compartilhar
+		</span>
+	</div>
 </div>
--- resources/views/post/status.blade.php
+++ resources/views/post/status.blade.php
@@ -3,6 +3,7 @@
 	<div class="row principal">
 		<div class="col-sm-2">
 			<div class="foto-label">
+				<img class="foto-avatar" src="{{ $Post->entidade->getAvatarUrl() }}" alt="{{ $Post->entidade->nome }}">
 				<div class="titulo-label">{{$Post->entidade->apelido }}</div>
 			</div>
 		</div>
