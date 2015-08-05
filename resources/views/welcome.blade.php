a6ad67022de4f3452411c7340d1b30927130a18f
Evandro Carreira <evandro.carreira@gmail.com>
2015-08-04 22:51:09 -0300

    Header do header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,18 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Vivalá</title>
 
-	<link href="{{ asset('/css/all.css') }}" rel="stylesheet">
-	
+	<link rel="icon" href="/favicon-16.png" sizes="16x16"/>
+    <link rel="icon" href="/favicon-32.png" sizes="32x32"/>
 
-	
+	<link href="{{ asset('/css/all.css') }}" rel="stylesheet">
+
+
+
 
 
 
 	<!-- Fonts -->
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:800,400' rel='stylesheet' type='text/css'>
+	<link href='fonts/futura/futura.css' rel='stylesheet' type='text/css'>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -24,41 +27,52 @@
 	<![endif]-->
 </head>
 <body>
-	<nav class="navbar welcome-navbar">
-		<div class="col-sm-4">
-			<a class="navbar-brand" href="{{ url('home') }}">Vivalá</a>
+	<div class="row welcome-header">
+		<div class="col-sm-5">
+			<a class="navbar-brand logo" href="{{ url('home') }}"><img src="/logo.png" alt="Vivalá" title="Vivalá"></a>
 		</div>
-		<div class="col-sm-8">
-	      		<div class="col-sm-3">
-					<label for='idioma'>IDIOMA</label>
-	      			<div class="input-wrapper select-wrapper">
-	      				{!! Form::select('idioma', array('pt' => 'PORTUGUÊS', 'en' => 'INGLÊS')) !!}
-	      			</div>
-	      		</div>
+		<div class="col-sm-7">
 				<div class="col-sm-8">
+					{!! Form::open(['url' => '/auth/login', 'class' => 'form-horizontal form-login']) !!}
+						<div class="row">
+							<div class="col-sm-4">
+								{!! Form::label('idioma', 'Idioma', ['class' => 'titulo']) !!}
 
-					<!-- Adiciona a abertura do Form -->
-					{!! Form::open(['url' => '/auth/login', 'class' => 'form-horizontal']) !!}
-							{!! Form::label('email', 'FAZER LOGIN') !!}
-			      			<div class="input-wrapper">
-								{!! Form::email("email", old('email'), ['class' => 'form-control', 'placeholder' => 'E-MAIL']) !!}
-			      			</div>
-			      			<div class="input-wrapper input-group">
-		      		  			{!! Form::password("password", ['class' => 'form-control', "placeholder" => "SENHA"]) !!}
-					      		<span class="input-group-btn">
-			      		  			{!! Form::submit("OK", ['class' => 'btn-default btn loginbtn']) !!}
-					      		</span>
-			      			</div>
-			      			<div class="checkbox">
-								<label>
-									<input type="checkbox" name="remember"> Mantenha-me logado
-								</label>
 							</div>
-							<a class="forgot-password" href="{{ url('/password/email') }}">Esqueceu a senha?</a>
+							<div class="col-sm-3">
+								{!! Form::label('email', 'Fazer login', ['class' => 'titulo']) !!}
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-sm-4">
+								{!! Form::select('idioma', array('pt' => 'PORTUGUÊS', 'en' => 'INGLÊS')) !!}
+							</div>
+							<div class="col-sm-4">
+								{!! Form::email("email", old('email'), ['class' => 'form-control', 'placeholder' => 'E-MAIL']) !!}
+							</div>
+							<div class="col-sm-4">
+								{!! Form::password("password", ['class' => 'form-control', "placeholder" => "SENHA"]) !!}
+								<span class="btn-submit">
+									{!! Form::submit("OK", ['class' => 'btn-default btn loginbtn']) !!}
+								</span>
+							</div>
+						</div>
+			      			<div class="row">
+								<div class="col-sm-4">
+									&nbsp;
+								</div>
+								<div class="col-sm-4">
+									<label class="rememberme" for="remember"><input type="checkbox" name="remember" id="remember">Mantenha-me logado</label>
+								</div>
+								<div class="col-sm-4">
+									<a href="{{ url('/password/email') }}">Esqueceu a senha?</a>
+								</div>
+							</div>
 					{!! Form::close() !!}
 				</div>
 		</div>
-	</nav>
+	</div>
 
 	<div class="row welcome-content">
 		<div class="container">
@@ -96,24 +110,24 @@
 								</div>
 							</div>
 						</div>
-					@endif		
+					@endif
 
 
 					<div class="form-group">
 						<div class="col-sm-6">
 		      				<!-- Adiciona um text field para o form-->
 							{!! Form::text("username", null, ['class' => 'form-control', 'placeholder' => 'NOME']) !!}
-						</div>						
+						</div>
 						<div class="col-sm-6">
 		      				<!-- Adiciona um text field para o form-->
-							{!! Form::text("username_last", null, ['class' => 'form-control', 'placeholder' => 'SOBRENOME']) !!}		      				
+							{!! Form::text("username_last", null, ['class' => 'form-control', 'placeholder' => 'SOBRENOME']) !!}
 						</div>
 					</div>
 
 					<div class="form-group">
 						<div class="col-sm-6">
 		      				<input type="text" class="form-control" placeholder="MASCULINO">
-						</div>						
+						</div>
 						<div class="col-sm-6">
 		      				<input type="text" class="form-control" placeholder="FEMININO">
 						</div>
@@ -136,16 +150,16 @@
 							{!! Form::email("email_confirmation", old('email'), ['class' => 'form-control', 'placeholder' => 'CONFIRMAÇÃO DE E-MAIL']) !!}
 						</div>
 					</div>
-					
+
 					<div class="form-group">
 						<div class="col-sm-12">
 		      				{!! Form::password("password", ['class' => 'form-control', "placeholder" => "SENHA"]) !!}
 						</div>
 					</div>
-					
+
 					<p>Ao clicar em confirmar, você concorda com nossos Termos e Política de Dados, incluindo nosso Uso de Cookies.</p>
 					<a href="{{ url('/auth/login') }}" class="anchor-tour">Fazer um tour pela Vivalá, sem se cadastrar.</a>
-					
+
 					{!!Form::submit("CONFIRMAR", ['class' => 'btn btn-default']) !!}
 
 				{!! Form::close() !!}
