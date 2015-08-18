f28fbf5ae59d6af3725b2a486244bbdd07cd99d2
Renato Gomes <renatoo.agomes@gmail.com>
2015-08-17 22:32:05 -0300

    Commit com Foto Cropandsave funcionando para todas as entidades

--- resources/views/ong/edit.blade.php
+++ resources/views/ong/edit.blade.php
@@ -6,8 +6,7 @@
 	<!-- Adiciona um formulario pra upload de foto-->
 	<div class="jc_coords row col-sm-12">
 
-	{!! Form::open(['url' => ['foto/uploadphoto',  $ong->id ], 'files' => true, 'onsubmit' => 'return verificaRecorteImagem(this);', 'class' => 'form-ajax']) !!}
-		{!! Form::hidden("tipoEntidade",  "App\Ong") !!} 	
+	{!! Form::open(['url' => ['foto/cropandsave',  $ong->id ], 'files' => true, 'onsubmit' => 'return verificaRecorteImagem(this);', 'class' => 'form-ajax']) !!}
 
 		{!! Form::label("image_file_upload", "Escolha outra imagem:") !!}
 		{!! Form::file("image_file_upload", ['id' => 'image_file_upload']) !!}
