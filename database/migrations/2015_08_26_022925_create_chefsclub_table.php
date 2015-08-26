f683961fab7929c6d5dcde95edf885bc41f77759
Rodrigo Ueda <rodrigoyueda@gmail.com>
2015-08-25 23:52:47 -0300

    ChefsClubSeeder

---
+++ database/migrations/2015_08_26_022925_create_chefsclub_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChefsclubTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('chefsclub', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('enedereco');
+			$table->string('tipo_cozinha');
+			$table->integer('preco');
+			$table->string('imagem');
+			$table->string('beneficio');
+			$table->string('horario');
+			$table->string('telefone');
+			$table->timestamps();
+		});
+
+		if (strcasecmp(env('DB_DRIVER'), 'pgsql') == 0) {
+			DB::statement('ALTER TABLE chefsclub ADD COLUMN posicao geometry(Point,4326) NULL;');
+        }
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('chefsclub');
+	}
+
+}
