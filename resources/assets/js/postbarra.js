683f1ecfa00a11ed94333465137c72fd71820500
Evandro Carreira <evandro.carreira@gmail.com>
2015-09-09 20:14:23 -0300

    Like de Post corrigido

--- resources/assets/js/postbarra.js
+++ resources/assets/js/postbarra.js
@@ -1,51 +1,53 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-    //Like
-    $(".like-btn").click(function(){
-        var href = $(this).prop("hash"),
-            link = href.substr(1),
-            urlArray = link.split('/'),
-            idPost = urlArray[2];
+  //Like
+  $(".like-btn-post").click(function() {
+    var href = $(this).prop("hash"),
+      link = href.substr(1),
+      urlArray = link.split('/'),
+      idPost = urlArray[2];
 
-        $.ajax({
-    		url: link
-    	})
-    	.done(function(data) {
-            var msgQtdCurtidas,
-                qtdLikes = data;
-            $("#barra-post-"+idPost+" .like-btn").addClass('liked');
-    	    if(qtdLikes > 1)
-    			msgQtdCurtidas = qtdLikes+' Curtidas';
-        	else if(qtdLikes == 1)
-    			msgQtdCurtidas = qtdLikes+' Curtida';
-        	else
-                msgQtdCurtidas = 'Curtir';
-            //Atualiza a quantidade de likes no span logo depois
-            $("#barra-post-"+idPost).find("span.qtd-likes").html(msgQtdCurtidas);
-    	})
-    	.fail(function(data) {
-    	  console.log('Erro no ajax de like');
-        });
-    });
+    $.ajax({
+        url: link
+      })
+      .done(function(data) {
+
+        console.log($("#barra-post-" + idPost).find(".like-btn-post+span.qtd-likes"));
+        var msgQtdCurtidas,
+          qtdLikes = data;
+        $("#barra-post-" + idPost + " .like-btn-post").addClass('liked');
+        if (qtdLikes > 1)
+          msgQtdCurtidas = qtdLikes + ' Curtidas';
+        else if (qtdLikes == 1)
+          msgQtdCurtidas = qtdLikes + ' Curtida';
+        else
+          msgQtdCurtidas = 'Curtir';
+        //Atualiza a quantidade de likes no span logo depois
+        $("#barra-post-" + idPost).find(".like-btn-post+span.qtd-likes").html(msgQtdCurtidas);
+      })
+      .fail(function(data) {
+        console.log('Erro no ajax de like');
+      });
+  });
 
 
-    //Share
-    $(".share-btn").click(function(){
-        var href = $(this).prop("hash"),
-            link = href.substr(1),
-            urlArray = link.split('/'),
-            idPost = urlArray[2];
+  //Share
+  $(".share-btn").click(function() {
+    var href = $(this).prop("hash"),
+      link = href.substr(1),
+      urlArray = link.split('/'),
+      idPost = urlArray[2];
 
-        $.ajax({
-            url: link
-        })
-        .done(function(data) {
-            location.reload();
-        })
-        .fail(function(data) {
-            alert('YOOOOU SHALL NOT SHARE THY POST!!!!!!');
-            console.log('Erro no ajax de share');
-        });
-    });
+    $.ajax({
+        url: link
+      })
+      .done(function(data) {
+        location.reload();
+      })
+      .fail(function(data) {
+        alert('YOOOOU SHALL NOT SHARE THY POST!!!!!!');
+        console.log('Erro no ajax de share');
+      });
+  });
 
 });
--- resources/views/post/_barrainfos.blade.php
+++ resources/views/post/_barrainfos.blade.php
@@ -1,25 +1,25 @@
 <div class="row barra-post fundo-cheio" id="barra-post-{{ $Post->id }}">
 
 	<div class="col-sm-4 tag">
-		<a href="#perfilcontroller/likepost/{{ $Post->id }}" class="like-btn"><i class="fa fa-tag {{ $Post->likedByMe() }}"></i></a>
+		<a href="#perfilcontroller/likepost/{{ $Post->id }}" class="tag-btn"><i class="fa fa-tag"></i></a>
 		<span class="qtd-likes">
 
 		</span>
 	</div>
 	<div class="col-sm-2 like">
-		<a href="#post/likepost/{{ $Post->id }}" class="like-btn {{ $Post->likedByMe() }}"><i class="fa fa-heart"></i></a>
+		<a href="#post/likepost/{{ $Post->id }}" class="like-btn like-btn-post {{ $Post->likedByMe() }}"><i class="fa fa-heart"></i></a>
 		<span class="qtd-likes">
 			@if($Post->getQuantidadeLikes() > 1)
 				 {{ $Post->getQuantidadeLikes() }} {{ trans("post.likes") }}
 			@elseif($Post->getQuantidadeLikes() == 1)
 				 {{ $Post->getQuantidadeLikes() }} {{ trans("post.like") }}
 			@else
-				{{ trans("post.like") }}
+				{{ trans("post.liker") }}
 			@endif
 		</span>
 	</div>
 	<div class="col-sm-2 comentario">
-		<a href="#post/likepost/{{ $Post->id }}" class="like-btn"><i class="fa fa-comment {{ $Post->likedByMe() }}"></i></a>
+		<a href="#post/likepost/{{ $Post->id }}" class="comment-btn"><i class="fa fa-comment {{ $Post->likedByMe() }}"></i></a>
 		<span class="qtd-likes">
 			0 {{ trans("post.comments") }}
 		</span>
