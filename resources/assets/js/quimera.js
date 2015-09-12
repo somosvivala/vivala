7cae0560892f3e348f770c2b96679896641bc261
Rodrigo Ueda <rodrigoyueda@gmail.com>
2015-09-11 23:08:13 -0300

    testes de ajax do quimera tunelados pelo vivala

--- resources/assets/js/quimera.js
+++ resources/assets/js/quimera.js
@@ -1,19 +1,19 @@
+'use strict';
 /**
  * Autocomplete para ser bindado no key up de inputs de viagens.
  * retorna um objeto "autocomplete" com nomes, tipos e códigos das localidades
  */
 var autocomplete = function(query) {
-    $.ajaxSetup({
-        headers: { 
-            'Access-Control-Allow-Origin': '*'
-        }
-    });
 
     $.ajax({
-        url: 'https://www.e-agencias.com.br/jano-flights/api/autocomplete/cities_airports',
-        type: 'GET',
+        url: '/quimera',
+        type: 'POST',
         dataType: 'json',
-        data: {query: query},
+        data: {
+            query: query, 
+            url: 'https://www.e-agencias.com.br/jano-flights/api/autocomplete/cities_airports',
+            method: 'get'
+        },
     })
     .done(function(data) {
         console.log(data);
@@ -25,19 +25,19 @@ var autocomplete = function(query) {
  */
 var searchTrip = function(params) {
     var
-        base_url      = 'https://www.e-agencias.com.br/vivala/search/flight',
+        base_url      = 'https://www.e-agencias.com.br/jano-flights/api/flights',
         defaultParams = {
             currency: 'BRL',
             site: 'BR',
             agency: 'vivala',
-            from: '',                /* 3 uppercased characters */
-            to: '',                  /* 3 uppercased characters */
-            departureDate: '',       /* YYYY-mm-dd */
-            returnDate: null,        /* YYYY-mm-dd */
-            adults: 0,               /* Quantidade de adultos */
-            infant: 0,               /* Quantidade de crianças até 11 anos */
-            children: 0,             /* Quantidade de crianças até 24 meses viajando no colo */
-            sortBy: null,            /* ['personal_ascending'|'duration_ascending'|'stopscount_ascending'|'stopscount_descending'|'total_price_ascending'|'total_price_descending'] */
+            from: '',                        /* 3 uppercased characters */
+            to: '',                          /* 3 uppercased characters */
+            departureDate: '',               /* YYYY-mm-dd */
+            returnDate: null,                /* YYYY-mm-dd */
+            adults: 0,                       /* Quantidade de adultos */
+            infant: 0,                       /* Quantidade de crianças até 11 anos */
+            children: 0,                     /* Quantidade de crianças até 24 meses viajando no colo */
+            sortBy: 'total_price_ascending', /* ['personal_ascending'|'duration_ascending'|'stopscount_ascending'|'stopscount_descending'|'total_price_ascending'|'total_price_descending'] */
             facet: {
                 stops: null,
                 airlines: null,
@@ -47,17 +47,12 @@ var searchTrip = function(params) {
 
     $.extend(defaultParams, params);
 
-    $.ajaxSetup({
-        headers: { 
-            'Access-Control-Allow-Origin': '*'
-        }
-    });
-
     $.ajax({
         url: base_url,
         type: 'GET',
-        dataType: 'json',
+        dataType: 'jsonp',
         data: defaultParams,
+        jsonpCallback: 'callback',
     })
     .done(function(data) {
         console.log(data);
@@ -83,7 +78,14 @@ var sortFilterTrip = function(select) {
     for (var i = 0; i < filters.length; i++) {
         var option = document.createElement("option");
         $(option).val(filters[i][0]).html(filters[i][1]);
+        if (filters[i][0] == 'total_price_ascending') {
+            $(option).prop('selected', true);
+        }
         $(select).append(option);
     }
     return select;
+};
+
+var getFlightCompanies = function(data) {
+    console.log(data);
 };
