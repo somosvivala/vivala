035b7f2958056572334b6f5f337b9de57bd689ef
Evandro Carreira <evandro.carreira@gmail.com>
2015-09-16 09:10:11 -0300

    Correção do css das foto de avatar

--- resources/views/feed/_comentarios.blade.php
+++ resources/views/feed/_comentarios.blade.php
@@ -3,7 +3,7 @@
 	@forelse($Post->novosComentariosByDate($qtdComentariosExibicao) as $Comentario)
         <li class="col-sm-12 post barra-comentarios" id="barra-comentario-{{ $Comentario->id }}">
 			<div class="col-sm-2">
-		        <img class="foto-avatar" src="{{ $Comentario->author->getAvatarUrl() }}" alt="{{ $Comentario->author->nome }}">
+		        <img class="foto-avatar-comentario" src="{{ $Comentario->author->getAvatarUrl() }}" alt="{{ $Comentario->author->nome }}">
 		    </div>
 			<div class="col-sm-8">
 				{{ $Comentario->conteudo }}
