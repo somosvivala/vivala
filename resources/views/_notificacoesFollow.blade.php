fc823b8950f9bccf88a8ede98c18a6075311180a
Evandro Carreira <evandro.carreira@gmail.com>
2015-09-15 21:45:54 -0300

    Arquivos de blade faltantes

---
+++ resources/views/_notificacoesFollow.blade.php
@@ -0,0 +1,26 @@
+ <ul class="col-sm-12">
+    @if(isset($notificacoes))
+    @forelse($notificacoes as $Notificacao)
+    <li class="row">
+        <div class="col-sm-4">
+            <a href="{{ url($Notificacao->from->getUrl()) }}">
+                <div class="round foto-avatar">
+                    <div class="cover">
+                        <img src="{{ $Notificacao->from->getAvatarUrl() }}" alt=" {{ $Notificacao->from->nome }}">
+                    </div>
+                </div>
+            </a>
+            {!! Form::close() !!}
+        </div>
+        <div class="col-sm-8 text-left">
+            <a href="{{ url($Notificacao->url) }}">
+                <strong class="font-bold-upper">{{ $Notificacao->titulo }}</strong>
+                <p>{{ $Notificacao->mensagem }}</p>
+            </a>
+        </div>
+    </li>
+    @empty
+        <p>Sem viajantes pra seguir! :o</p>
+    @endforelse
+    @endif
+</ul> 
