3a1a9c583d8c9a0830039f269bd95cfcc626d8dc
Rodrigo Ueda <rodrigoyueda@gmail.com>
2015-09-20 20:37:03 -0300

    layout de hoteis

--- resources/views/quimera/_hotels.blade.php
+++ resources/views/quimera/_hotels.blade.php
@@ -1,5 +1,6 @@
 @foreach ($data as $hotel)
-    <div class="col-sm-6 quimera-hotel" style="background-image: url({!! $hotel->picture->url !!});">
-        
+    <div class="col-sm-4">
+        <div class="quimera-hotel" style="background-image: url({!! $hotel->picture->url !!});">
+        </div>
     </div>
 @endforeach
