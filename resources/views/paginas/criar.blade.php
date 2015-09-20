940cb3fa584005936925c926bb45dc6bf8eb1a91
Evandro Carreira <evandro.carreira@gmail.com>
2015-09-20 15:57:05 -0300

    Style de criar página

--- resources/views/paginas/criar.blade.php
+++ resources/views/paginas/criar.blade.php
@@ -11,48 +11,52 @@
         <div class="box-criar">
             <img src="/images/iconcriarempresa.png" alt="Criar Empresa">
             <h4>Empresa</h4>
-        </div>
-        <div class="form-criar">
-            {!! Form::open(array('url' => array('empresa/create')))!!}
-            <span> Junte-se as pessoas que apoiam você na Vivalá! </span>
-            @if(isset($categoriasEmpresas))
-            <select name="categoriaEmpresa" class="suave">
-                <option value="null">Escolha uma categoria</option>
-            @forelse($categoriasEmpresas as $Categoria)
-                <option value="{{ $Categoria->id }}">{{ $Categoria->nome }}</option>
-            @empty
-                <option>Sem categorias</option>
-            @endforelse
-            </select>
-            @endif
-            {!! Form::text("nome",  null , ['class' => 'form-control suave', 'placeholder'=>'Nome da Empresa']) !!} 
-            <span>Ao clicar em Começar, você concorda com os <a href="{{ url('PaginaController@getTermos') }}">Termos das Páginas da Vivalá</a></span>        
-            {!! Form::submit('Começar', ['class' => 'btn']) !!}
-            {!! Form::close() !!}
+            <div class="form-criar">
+                {!! Form::open(array('url' => array('empresa/create')))!!}
+                <span> Junte-se as pessoas que apoiam você na Vivalá! </span>
+                @if(isset($categoriasEmpresas))
+                <select name="categoriaEmpresa" class="suave">
+                    <option value="null">Escolha uma categoria</option>
+                @forelse($categoriasEmpresas as $Categoria)
+                    <option value="{{ $Categoria->id }}">{{ $Categoria->nome }}</option>
+                @empty
+                    <option>Sem categorias</option>
+                @endforelse
+                </select>
+                @endif
+                {!! Form::text("nome",  null , ['class' => 'suave', 'placeholder'=>'Nome da Empresa']) !!} 
+                <span>Ao clicar em Começar, você concorda com os <a href="{{ url('PaginaController@getTermos') }}">Termos das Páginas da Vivalá</a></span>        
+                <div class="col-sm-12 text-center">
+                    {!! Form::submit('Começar', ['class' => 'btn']) !!}
+                </div>
+                {!! Form::close() !!}
+            </div>
         </div>
     </div>
     <div class="col-sm-4">
         <div class="box-criar">
             <img src="/images/iconcriarong.png" alt="Criar Projeto Social">
             <h4>Projeto Social</h4>
-        </div>
-        <div class="form-criar">
-            {!! Form::open(array('url' => array('ong/create')))!!}
-            <span> Junte-se as pessoas que apoiam você na Vivalá! </span>
-            @if(isset($categoriasOngs))
-            <select name="categoriaOng" class="suave">
-                <option value="null">Escolha uma categoria</option>
-            @forelse($categoriasOngs as $Categoria)
-                <option value="{{ $Categoria->id }}">{{ $Categoria->nome }}</option>
-            @empty
-                <option>Sem categorias</option>
-            @endforelse
-            </select>
-            @endif
-            {!! Form::text("nome",  null , ['class' => 'form-control suave', 'placeholder'=>'Nome da Empresa']) !!} 
-            <span>Ao clicar em Começar, você concorda com os <a href="{{ url('PaginaController@getTermos') }}">Termos das Páginas da Vivalá</a></span>        
-            {!! Form::submit('Começar', ['class' => 'btn']) !!}
-            {!! Form::close() !!}
+            <div class="form-criar">
+                {!! Form::open(array('url' => array('ong/create')))!!}
+                <span> Junte-se as pessoas que apoiam você na Vivalá! </span>
+                @if(isset($categoriasOngs))
+                <select name="categoriaOng" class="suave">
+                    <option value="null">Escolha uma categoria</option>
+                @forelse($categoriasOngs as $Categoria)
+                    <option value="{{ $Categoria->id }}">{{ $Categoria->nome }}</option>
+                @empty
+                    <option>Sem categorias</option>
+                @endforelse
+                </select>
+                @endif
+                {!! Form::text("nome",  null , ['class' => 'suave', 'placeholder'=>'Nome da Empresa']) !!} 
+                <span>Ao clicar em Começar, você concorda com os <a href="{{ url('PaginaController@getTermos') }}">Termos das Páginas da Vivalá</a></span>        
+                <div class="col-sm-12 text-center">
+                    {!! Form::submit('Começar', ['class' => 'btn']) !!}
+                </div>
+                {!! Form::close() !!}
+            </div>
         </div>
     </div>
     <div class="col-sm-4">
