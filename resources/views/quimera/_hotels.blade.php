cb884e7f6798878f8ed52b69c842d2558c2bd419
Rodrigo Ueda <rodrigoyueda@gmail.com>
2015-09-20 21:52:10 -0300

    layout de listagem de hoteis - preview

--- resources/views/quimera/_hotels.blade.php
+++ resources/views/quimera/_hotels.blade.php
@@ -1,6 +1,16 @@
+{{-- $hotel->roompacks->priceDetail->nightly->subtotal --}}
 @foreach ($data as $hotel)
     <div class="col-sm-4">
         <div class="quimera-hotel" style="background-image: url({!! $hotel->picture->url !!});">
+        </div>
+        <div class="hotel-header">
+            <div class="hotel-price">
+                R$ {{$hotel->roompacks[0]->priceDetail->nightly->subtotal}}
+            </div>
+            <div class="hotel-like"> <i class="fa fa-heart"></i> </div>
+        </div>
+        <div class="hotel-footer">
+            {{strtoupper($hotel->hotel->name)}}
         </div>
     </div>
 @endforeach
