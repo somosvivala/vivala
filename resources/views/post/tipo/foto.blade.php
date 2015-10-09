910bcd13291c34e9bda8973fa94e575593b62892
Evandro Carreira <evandro.carreira@gmail.com>
2015-10-09 19:20:44 -0300

    Feature: Remover post na tela de detalhes do post

--- resources/views/post/tipo/foto.blade.php
+++ resources/views/post/tipo/foto.blade.php
@@ -1,5 +1,12 @@
 <article class="foto">
-	<div class="hora-post"> {{ $Post->data_postagem_diff }} </div>
+    <div class="hora-post"> {{ $Post->data_postagem_diff }}[
+        @if ( Auth::user()->entidadeAtiva == $Post->entidade )
+                {!! Form::open([ 'method' => 'DELETE', 'route' => ['post.destroy', $Post->id] ]) !!}
+                    {!! Form::submit('Remover ', ['class' => 'btn', 'onclick' => "return confirm('Tem certeza que deseja remover esse post?');"]) !!}
+            
+                {!! Form::close() !!}
+        @endif
+         </div>
 	<div class="row principal foto-fundo"
 	@if($Post->fotos)
 	 style="background-image:url('{{ $Post->fotos->path }}')"
