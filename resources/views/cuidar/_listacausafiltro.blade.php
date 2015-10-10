1f056edccd6ddbc017491d2994c8ed5f055c6429
Evandro Carreira <evandro.carreira@gmail.com>
2015-10-09 21:03:03 -0300

    Css de lista de causa; botão de criar vaga na busca de vaga

--- resources/views/cuidar/_listacausafiltro.blade.php
+++ resources/views/cuidar/_listacausafiltro.blade.php
@@ -1,11 +1,13 @@
-<ul class="lista-ongs row">
+<ul class="lista-vagas row">
 
     @if(isset($causas))
     @forelse($causas as $Causa)
         <li class="col-sm-4">
             <div class="foto-fundo" style="background-image:url('{{ $Causa->getCapaUrl() }}');">
-            <a href="{{ url('causa/sobre/'.$Causa->id) }}">
-                <h3 class="font-bold-upper">{{ $Causa->nome }}</h3>
+            <a href="{{ url('vagas/'.$Causa->id) }}">
+                <h3 class="font-bold-upper">{{ $Causa->owner->nome }}</h3>
+                <p>{{ $Causa->habilidades }}</p>
+                <span class="padding-t-1"><i class="fa fa-map-marker"></i> {{ $Causa->cidade->nome.",".$Causa->estado->sigla }}</span>
             </a>
         </li>
     @empty
--- resources/views/cuidar/vagas.blade.php
+++ resources/views/cuidar/vagas.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <div class="fundo-cheio col-sm-12 margin-b-2 padding-b-2">
+    <div class="absolute-top-right">
+        <a href="{{ url('vagas/create') }}"  class="padding-btn suave ">Criar vaga</a>
+    </div>
     <h3 class="font-bold-upper text-center"> Procurando por causas?
         <small class="sub-titulo">Encontre em 3 passos.</small>
     </h3>
