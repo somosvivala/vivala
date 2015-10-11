37ce2119c1336f229a0c6f10ed4cecfde4f91860
Renato Gomes <renatoo.agomes@gmail.com>
2015-10-11 19:58:00 -0300

    commit com inicio da configuracao de email

--- config/mail.php
+++ config/mail.php
@@ -28,7 +28,7 @@ return [
 	|
 	*/
 
-	'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+	'host' => env('MAIL_HOST', 'mail.vivalabrasil.com.br'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'noreply@vivalabrasil.com.br', 'name' => 'Vivalá'],
 
 	/*
 	|--------------------------------------------------------------------------
