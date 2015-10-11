f444face16f31ead251133c34518162644eb941d
Evandro Carreira <evandro.carreira@gmail.com>
2015-10-11 01:08:56 -0300

    Rodapé com links de about

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,32 @@
-<div class="container">
-  	<nav>
-    	<a href="//www.iubenda.com/privacy-policy/964644" class="iubenda-black iubenda-embed" title="Privacy Policy">Privacy Policy</a>
-	</nav>
+<div class="col-sm-12 fundo-cheio margin-t-1 padding-t-1 footer">
+    <div class="col-sm-12 col-sm-offset-0 col-md-8 col-md-offset-2">
+        <div class="col-sm-3 text-right">
+            <span class="font-bold-upper">
+                Mais sobre a gente
+            </span>
+        </div>
+        <div class="col-sm-3 text-left">
+            <ul>
+                <li><a href="#"><i class="fa fa-plus"></i> nosso manifesto</a></li>
+                <li><a href="#"><i class="fa fa-plus"></i> quem somos</a></li>
+                <li><a href="#"><i class="fa fa-plus"></i> o que fazemos</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-3 text-right">
+            <span class="font-bold-upper">
+                Informações
+            </span>
+        </div>
+        <div class="col-sm-3 text-left">
+            <ul>
+                <li><a href="#"><i class="fa fa-plus"></i> contato</a></li>
+                <li><a href="#"><i class="fa fa-plus"></i> termos e condições</a></li>
+                <li><a href="#"><i class="fa fa-plus"></i> política de privacidade</a></li>
+                <li><a href="#"><i class="fa fa-plus"></i> parceiros</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="col-sm-12 margin-t-1 margin-b-1 text-center">
+Todos os direitos reservados Vivalá Turismo LTDA - CNPJ 22.693.622/0001-98
 </div>
