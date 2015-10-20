cccfdb9429c329b5046c01a00203ffdbb3e15c9e
Evandro Carreira <evandro.carreira@gmail.com>
2015-10-20 01:58:34 -0200

    Css de detalhes de hotel

--- resources/views/quimera/_hotelDetail.blade.php
+++ resources/views/quimera/_hotelDetail.blade.php
@@ -12,40 +12,44 @@
 
     <div class="fotos-detalhe">
         @foreach($data['extra']->pictures as $foto)
-        <img src="{{ $foto->url}}/50x50?truncate=true">
+        <a href="{{$foto->url}}" data-lightbox="hotel-detalhes">
+            <img src="{{ $foto->url}}/50x50?truncate=true">
+        </a>
         @endforeach
     </div>
 
-    <div class="descricao padding-t-2">
-        <p>{{ $data['extra']->information }}</pre>
+    <div class="descricao padding-t-2 margin-b-2">
+        <p>{!! nl2br($data['extra']->information) !!}</p>
     </div>
 
-    {{ count($data['data']->roomClusters) }} Opções disponíveis
+    <h5 class="font-bold-upper text-left    "> {{ count($data['data']->roomClusters) }} Opções disponíveis </h5>
 
     @foreach($data['data']->roomClusters as $roomCluster)
-    <div class="row">
+    <div class="col-sm-12 margin-b-2">
 
-        <div class="room-cluster col-sm-12">
-            <div class="col-sm-6">
-                <div class="col-sm-4">
-                    <img src="{{ $roomCluster->roompacks[0]->rooms[0]->roomDetails->pictures[0]->url }}/100x80?truncate=true">
-                </div>
-                <div class="col-sm-8">
-                    <h4 class="font-bold-upper">{{ $roomCluster->roompacks[0]->rooms[0]->roomDetails->despegarName }}</h4>
+        <div class="room-cluster row ">
+            <div class="col-sm-6 padding-t-1">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <img src="{{ $roomCluster->roompacks[0]->rooms[0]->roomDetails->pictures[0]->url }}/100x80?truncate=true">
+                    </div>
+                    <div class="col-sm-8">
+                        <h5 class="font-bold-upper">{{ $roomCluster->roompacks[0]->rooms[0]->roomDetails->despegarName }}</h5>
+                    </div>
                 </div>
             </div>
 
-            <div class="col-sm-6">
-            @foreach($roomCluster->roompacks as $roompack)
-                <div class="row padding-t-1 padding-b-1">
+            <div class="col-sm-6 roompacks">
+                @foreach($roomCluster->roompacks as $roompack)
+                <div class="row roompack">
                     <div class="col-sm-7">
-                        <ul>
-                            <li> {{ $roompack->mealPlan->description }}</li>
-                            <li> {{ $roompack->cancellationPolicy->text }}</li>
+                        <ul class="padding-t-1">
+                            <li>- {{ $roompack->mealPlan->description }}</li>
+                            <li>- {{ $roompack->cancellationPolicy->text }}</li>
                         </ul>
                     </div>
 
-                    <div class="col-sm-5">
+                    <div class="col-sm-5 tarifa padding-t-1 padding-b-1 ">
                         <span class="font-bold-upper">Tarifa por Noite</span>
                         <h5 class="font-bold-upper"> R$ {{ $roompack->paymentTypes->prepaid->priceDetail->nightly->subtotal }} </h5>
                         <a class="btn btn-primario" href="#" data-choice="{{ $roompack->choice }}">
@@ -55,16 +59,24 @@
                         @endforeach
                     </div>
                 </div>
-            @endforeach
+                @endforeach
             </div>
         </div>
     </div>
     @endforeach
+
+    <h5 class="font-bold-upper text-left">Serviços da {{ $data['extra']->name }}</h5>
+    <div class="amenities row">
+        @foreach($data['extra']->amenities as $Amenity)
+        <span class="col-md-4 col-sm-6 col-xs-12 servico">
+            - {{ $Amenity->description }}
+        </span>
+        @endforeach
+    </div>
 </div>
 <?php 
 /**
  *  data =>  dados de disponibilidade e informações de quartos
  *  extra => Informações sobre o hotel
  */ 
-?>
-<?php dd($data['data'], $data['extra']); ?>
+dd($data['data'], $data['extra']); 
