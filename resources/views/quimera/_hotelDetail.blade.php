f4301d4d6695ec8f8aa03484e7ba4245741ec10f
Evandro Carreira <evandro.carreira@gmail.com>
2015-10-19 23:34:19 -0200

    Mostrando dados dos quartos e início do front

--- resources/views/quimera/_hotelDetail.blade.php
+++ resources/views/quimera/_hotelDetail.blade.php
@@ -1,30 +1,63 @@
-<div>
+<div class="hotel-detalhes">
     <div class="foto-fundo foto-header" style="background-image:url('{{ $data['extra']->pictures[0]->url}}/720x400?truncate=true');">
         <h2>{{ $data['extra']->name }}</h2>
+        <div class="quarto-destaque">
+            <span>{{ $data['data']->roomClusters[0]->roompacks[0]->rooms[0]->name }}</span>
+            <h5 class="font-bold-upper">R$ {{ $data['data']->roomClusters[0]->roompacks[0]->paymentTypes->prepaid->priceDetail->nightly->subtotal }}</h5>
+            <a class="btn btn-primario" href="#">
+                Comprar
+            </a>
+        </div>
     </div>
-    @foreach($data['extra']->pictures as $foto)
-    <img src="{{ $foto->url}}/50x50?truncate=true">
-    @endforeach
-    <div class="">
+
+    <div class="fotos-detalhe">
+        @foreach($data['extra']->pictures as $foto)
+        <img src="{{ $foto->url}}/50x50?truncate=true">
+        @endforeach
+    </div>
+
+    <div class="descricao padding-t-2">
         <p>{{ $data['extra']->information }}</pre>
     </div>
 
     {{ count($data['data']->roomClusters) }} Opções disponíveis
 
     @foreach($data['data']->roomClusters as $roomCluster)
-    <?php $details = $roomCluster->roompacks[0]->rooms[0]->roomDetails; ?>
-    <div class="room-cluster">
-    <pre>
-        <?php var_dump($details); ?>
-    </pre>
-        @foreach($roomCluster->roompacks as $roompack)
-            @foreach($roompack->rooms as $room)
-            {{ $room->name }}<br>
-            {{ $room->roomCode }}<br>
-            {{ $room->roomTypeId }}<br>
-            {{ $room->availability }}<br>
+    <div class="row">
+
+        <div class="room-cluster col-sm-12">
+            <div class="col-sm-6">
+                <div class="col-sm-4">
+                    <img src="{{ $roomCluster->roompacks[0]->rooms[0]->roomDetails->pictures[0]->url }}/100x80?truncate=true">
+                </div>
+                <div class="col-sm-8">
+                    <h4 class="font-bold-upper">{{ $roomCluster->roompacks[0]->rooms[0]->roomDetails->despegarName }}</h4>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+            @foreach($roomCluster->roompacks as $roompack)
+                <div class="row padding-t-1 padding-b-1">
+                    <div class="col-sm-7">
+                        <ul>
+                            <li> {{ $roompack->mealPlan->description }}</li>
+                            <li> {{ $roompack->cancellationPolicy->text }}</li>
+                        </ul>
+                    </div>
+
+                    <div class="col-sm-5">
+                        <span class="font-bold-upper">Tarifa por Noite</span>
+                        <h5 class="font-bold-upper"> R$ {{ $roompack->paymentTypes->prepaid->priceDetail->nightly->subtotal }} </h5>
+                        <a class="btn btn-primario" href="#" data-choice="{{ $roompack->choice }}">
+                            Comprar
+                        </a>
+                        @foreach($roompack->rooms as $room)
+                        @endforeach
+                    </div>
+                </div>
             @endforeach
-        @endforeach
+            </div>
+        </div>
     </div>
     @endforeach
 </div>
