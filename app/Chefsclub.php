a03d76e125a1a2ba09426aa9fb43da2dcf257fc7
Evandro Carreira <evandro@vivalabrasil.com.br>
2015-10-21 22:01:18 -0400

    Correção do array de cidades

--- app/Chefsclub.php
+++ app/Chefsclub.php
@@ -25,25 +25,25 @@ class Chefsclub extends Model {
     public static function getCidadeForSelect()
     {
         return array(
-            ['id' => 2,  'cidade' => 'Rio de Janeiro',              'estado' = 'RJ'],
-            ['id' => 3,  'cidade' => 'Porto Alegre',                'estado' = 'RS'],
-            ['id' => 4,  'cidade' => 'Baixada Fluminense',          'estado' = 'RJ'],
-            ['id' => 5,  'cidade' => 'Brasília',                    'estado' = 'DF'],
-            ['id' => 7,  'cidade' => 'Niteroi',                     'estado' = 'RJ'],
-            ['id' => 10, 'cidade' => 'Belo Horizonte',              'estado' = 'MG'],
-            ['id' => 12, 'cidade' => 'Balneário Camburiú e Região', 'estado' = 'SC'],
-            ['id' => 13, 'cidade' => 'São Paulo',                   'estado' = 'SP'],
-            ['id' => 14, 'cidade' => 'Curitiba',                    'estado' = 'PR'],
-            ['id' => 15, 'cidade' => 'Salvador',                    'estado' = 'BA'],
-            ['id' => 18, 'cidade' => 'Ribeirão Preto',              'estado' = 'SP'],
-            ['id' => 20, 'cidade' => 'Florianópolis',               'estado' = 'SC'],
-            ['id' => 21, 'cidade' => 'Fortaleza',                   'estado' = 'CE'],
-            ['id' => 22, 'cidade' => 'Recife',                      'estado' = 'PE'],
-            ['id' => 23, 'cidade' => 'Joinville',                   'estado' = 'SC'],
-            ['id' => 24, 'cidade' => 'Criciúma',                    'estado' = 'SC'],
-            ['id' => 25, 'cidade' => 'Vitória e Região',            'estado' = 'RJ'],
-            ['id' => 27, 'cidade' => 'Goiânia',                     'estado' = 'GO'],
-            ['id' => 29, 'cidade' => 'Campinas',                    'estado' = 'SP']
+            array('id' => 2,  'cidade' => 'Rio de Janeiro',              'estado' => 'RJ'),
+            array('id' => 3,  'cidade' => 'Porto Alegre',                'estado' => 'RS'),
+            array('id' => 4,  'cidade' => 'Baixada Fluminense',          'estado' => 'RJ'),
+            array('id' => 5,  'cidade' => 'Brasília',                    'estado' => 'DF'),
+            array('id' => 7,  'cidade' => 'Niteroi',                     'estado' => 'RJ'),
+            array('id' => 10, 'cidade' => 'Belo Horizonte',              'estado' => 'MG'),
+            array('id' => 12, 'cidade' => 'Balneário Camburiú e Região', 'estado' => 'SC'),
+            array('id' => 13, 'cidade' => 'São Paulo',                   'estado' => 'SP'),
+            array('id' => 14, 'cidade' => 'Curitiba',                    'estado' => 'PR'),
+            array('id' => 15, 'cidade' => 'Salvador',                    'estado' => 'BA'),
+            array('id' => 18, 'cidade' => 'Ribeirão Preto',              'estado' => 'SP'),
+            array('id' => 20, 'cidade' => 'Florianópolis',               'estado' => 'SC'),
+            array('id' => 21, 'cidade' => 'Fortaleza',                   'estado' => 'CE'),
+            array('id' => 22, 'cidade' => 'Recife',                      'estado' => 'PE'),
+            array('id' => 23, 'cidade' => 'Joinville',                   'estado' => 'SC'),
+            array('id' => 24, 'cidade' => 'Criciúma',                    'estado' => 'SC'),
+            array('id' => 25, 'cidade' => 'Vitória e Região',            'estado' => 'RJ'),
+            array('id' => 27, 'cidade' => 'Goiânia',                     'estado' => 'GO'),
+            array('id' => 29, 'cidade' => 'Campinas',                    'estado' => 'SP')
         );
     }
 }
