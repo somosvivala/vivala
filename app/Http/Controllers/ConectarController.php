9588f674a3070a0e1a4f8c1e7f9b50a49b6707c1
Renato Gomes <renatoo.agomes@gmail.com>
2015-10-22 16:10:59 -0200

    Commit com feature para se fixar um post, pasta adicionar ao .env a config POST_FIXO_ID= do post a ser fixado. Se a conf nao existir, nao fixa nenhum post

--- app/Http/Controllers/ConectarController.php
+++ app/Http/Controllers/ConectarController.php
@@ -11,27 +11,35 @@ use Session;
 
 class ConectarController extends VivalaBaseController {
 
-	public function __construct(){
-		//Só passa se estiver logado
-		$this->middleware('auth');
-	}
+    public function __construct(){
+        //Só passa se estiver logado
+        $this->middleware('auth');
+    }
 
-	// Compartilha as sugestões com as views que forem chamadas por esse controller
-	public function getSugestoesViajantes($view){
-		$sugestoesViajantes = Perfil::getSugestoes(Auth::user()->entidadeAtiva);
-		$view->with('sugestoesViajantes', $sugestoesViajantes);
-	}
+    // Compartilha as sugestões com as views que forem chamadas por esse controller
+    public function getSugestoesViajantes($view){
+        $sugestoesViajantes = Perfil::getSugestoes(Auth::user()->entidadeAtiva);
+        $view->with('sugestoesViajantes', $sugestoesViajantes);
+    }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$posts = Post::getUltimos();
-		return view('conectar.index', compact('posts'));
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $posts = Post::getUltimos()->keyBy('id');
 
-	}
+        if (env('POST_FIXO_ID') != null)
+        {
+            $post_fixo = $posts->find(env('POST_FIXO_ID'));
+            $posts->forget($post_fixo->id);
+            $posts->prepend($post_fixo);
+        }
+
+        return view('conectar.index', compact('posts'));
+
+    }
 
 }
