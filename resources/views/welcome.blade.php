012fd987568218094a5cf88426bae04a0caeb58b
Evandro Carreira <evandro@vivalabrasil.com.br>
2015-10-23 18:08:51 -0400

    fix css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,9 +94,12 @@
 						<h3>{{ trans('global.lbl_signup1') }}</h3>
 					</div>
 					<div class="col-md-6">
+                                            &nbsp;
+                                            {{--
 						<a href="{{ url('/fbLogin') }}" class="btn-social btn-facebook btn-xs">
 							<i class="fa fa-facebook"></i> {{ trans('global.fb_login') }}
 						</a>
+                                                --}}
 					</div>
 				</div>
 				@if (count($errors) > 0)
