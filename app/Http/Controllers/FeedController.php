6c6fb8d2b0d4e9dda9121271fa42b1ded7163eca
Renato Gomes <renatoo.agomes@gmail.com>
2015-10-24 03:51:58 -0200

    Commit para teste mais rapido

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -28,7 +28,7 @@ class FeedController extends Controller {
     {
         $posts = Post::getMaisfotos()->keyBy('id');
         $posts_total = count($posts);
-        $posts = $posts->slice($pagina, env('QUANTIDADE_FEED_POST'), true);
+        $posts = $posts->slice($pagina*env('QUANTIDADE_FEED_POST'), env('QUANTIDADE_FEED_POST'), true);
         $pagina++;
         return view('feed', compact('posts', 'pagina'));
     }
