c6ee9d8827f2ca153c1d8ff3039dcccae0fd87db
Evandro Carreira <evandro@vivalabrasil.com.br>
2015-10-23 23:16:56 -0400

    Lixeira no feed pra excluir posts; Reset password melhorado

--- resources/views/feed/acontecimento.blade.php
+++ resources/views/feed/acontecimento.blade.php
@@ -14,6 +14,12 @@
 	</div>
 	<div class="hora-post">
 		<a href="{{ $Post->url }}"> {{ $Post->data_postagem_diff }} </a>
+        @if ( Auth::user()->id == $Post->entidade->user->id )
+                    {!! Form::open([ 'method' => 'DELETE', 'route' => ['post.destroy', $Post->id] ]) !!}
+                    {!! Form::button('<i class="fa fa-trash "></i> ', ['type' => 'submit', 'class' => 'btn-icon', 'onclick' => "return confirm('Tem certeza que deseja remover esse post?');"]) !!}
+                
+                    {!! Form::close() !!}
+            @endif
 	</div>
 	@include('post._barrainfos')
 		<div class="row box-post">
--- resources/views/post/tipo/acontecimento.blade.php
+++ resources/views/post/tipo/acontecimento.blade.php
@@ -26,7 +26,7 @@
         {{ $Post->data_postagem_diff }}
         @if ( Auth::user()->id == $Post->entidade->user->id )
                 {!! Form::open([ 'method' => 'DELETE', 'route' => ['post.destroy', $Post->id] ]) !!}
-                    {!! Form::submit('Remover ', ['class' => 'btn', 'onclick' => "return confirm('Tem certeza que deseja remover esse post?');"]) !!}
+                    {!! Form::button('<i class="fa fa-trash "></i> ', ['type' => 'submit', 'class' => 'btn-icon', 'onclick' => "return confirm('Tem certeza que deseja remover esse post?');"]) !!}
             
                 {!! Form::close() !!}
         @endif
