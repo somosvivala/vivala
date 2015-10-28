1e05c05fd9b3e4602cc6b776aecd27d02ab140fd
Evandro Carreira <evandro.carreira@gmail.com>
2015-10-28 02:27:22 -0200

    Adicionando foto de perfil com cropper no quiz

--- resources/assets/js/cropFoto.js
+++ resources/assets/js/cropFoto.js
@@ -49,4 +49,66 @@ $(function() {
         });
     });
 
+    // Ativa crop do quiz
+    var $image = $('#cropper-quiz > img'),
+        cropBoxData,
+        canvasData;
+
+    $('#cropper-quiz-modal').on('shown.bs.modal', function () {
+        $image.cropper({
+            autoCropArea: 1,
+            aspectRatio: 1,
+            built: function () {
+                // Strict mode: set crop box data first
+                $image.cropper('setCropBoxData', cropBoxData);
+                $image.cropper('setCanvasData', canvasData);
+            },
+            guides: false,
+            crop: function(e) {
+                $("#x-quiz-perfil").val(Math.round(e.x));
+                $("#y-quiz-perfil").val(Math.round(e.y));
+                $("#h-quiz-perfil").val(Math.round(e.height));
+                $("#w-quiz-perfil").val(Math.round(e.width));
+                $("#r-quiz-perfil").val(e.rotate);
+                console.log(e);
+            }
+        });
+    }).on('hidden.bs.modal', function () {
+        cropBoxData = $image.cropper('getCropBoxData');
+        canvasData = $image.cropper('getCanvasData');
+        $image.cropper('destroy');
+    });
+
+    // Import image
+    var $inputImage = $('#input-quiz-foto-perfil');
+    var URL = window.URL || window.webkitURL;
+    var blobURL;
+
+    if (URL) {
+      $inputImage.change(function () {
+        var files = this.files;
+        var file;
+
+        if (!$image.data('cropper')) {
+          return;
+        }
+
+        if (files && files.length) {
+          file = files[0];
+
+          if (/^image\/\w+$/.test(file.type)) {
+            blobURL = URL.createObjectURL(file);
+            $image.one('built.cropper', function () {
+              URL.revokeObjectURL(blobURL); // Revoke when load complete
+            }).cropper('reset').cropper('replace', blobURL);
+//            $inputImage.val('');
+          } else {
+            $body.tooltip('Please choose an image file.', 'warning');
+          }
+        }
+      });
+    } else {
+      $inputImage.prop('disabled', true).parent().addClass('disabled');
+    }
+
 });
--- resources/views/quiz/personalizefoto.blade.php
+++ resources/views/quiz/personalizefoto.blade.php
@@ -2,34 +2,56 @@
 
 @section('pergunta')
 
-	<div class="col-sm-12 pergunta">
-		<span class="absolute-top-right laranja">
-			<a href="{{ url ('quiz')}}" class="absolute-top-left laranja">{{ trans("global.lbl_step_back") }}</a> | <a href="{{ url ('quiz/contemais')}}" class="laranja">{{ trans("global.lbl_step_skip") }}</a>
-		</span>
-		<h2>{{ trans("global.quiz_profile_customize") }}</h2>
-		<h3>{{ trans("global.quiz_photo_customize") }}</h3>
+<div class="col-sm-12 pergunta">
+    <span class="absolute-top-right laranja">
+        <a href="{{ url ('quiz')}}" class="absolute-top-left laranja">{{ trans("global.lbl_step_back") }}</a> | <a href="{{ url ('quiz/contemais')}}" class="laranja">{{ trans("global.lbl_step_skip") }}</a>
+    </span>
+    <h2>{{ trans("global.quiz_profile_customize") }}</h2>
+    <h3>{{ trans("global.quiz_photo_customize") }}</h3>
 
-		{!! Form::open(['url' => ['foto/cropandsave',  Auth::user()->perfil->id ], 'files' => true, 'data-redirect' => '/quiz/contemais', 'id'=>'quiz_foto_form', 'data-loading'=>'form-loading']) !!}
-		{!! Form::hidden("tipoEntidade",  "App\Perfil") !!}
+    <a type="button" data-target="#cropper-quiz-modal" data-toggle="modal">
 
-			<img id="preview" src="{{ $foto?$foto:'/img/interrogacao.png' }}" class="foto-preview"/>
+        <img src="{{ $foto?$foto:'/img/interrogacao.png' }}"/>
 
-			<div class="file-upload">
-				<label for="image_file_upload">
-					{{ trans("global.lbl_photo_send") }}
-					<p>{{ trans("global.quiz_fromcomputer") }}</p>
-					{!! Form::file("image_file_upload", ['id' => 'image_file_upload', 'class' => 'upload']) !!}
-				</label>
-			</div>
-				{!! Form::hidden("x",  0, ['id' => 'xJcropPerfil']) !!}
-				{!! Form::hidden("y",  0, ['id' => 'yJcropPerfil']) !!}
-				{!! Form::hidden("w",  0, ['id' => 'wJcropPerfil']) !!}
-				{!! Form::hidden("h",  0, ['id' => 'hJcropPerfil']) !!}
-				{!! Form::hidden("_token",  csrf_token(), ['name' => '_token' ]) !!}
-			<div class="erros">
-			</div>
-			{!! Form::submit( trans("global.lbl_continue"), ['class' => 'btn btn-primario btn-acao']) !!}
-                        <i id="form-loading" class="fa fa-spinner fa-pulse fa-2x " style="display:none"></i>
-		{!! Form::close() !!}
-	</div>
+        <div class="row margin-t-2">
+            <div class="file-upload">
+                <label for="image_file_upload">
+                    {{ trans("global.lbl_photo_send") }}
+                    <p>{{ trans("global.quiz_fromcomputer") }}</p>
+                </label>
+            </div>
+        </div>
+    </a>
+    <div class="modal fade" id="cropper-quiz-modal">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-body">
+                    {!! Form::open(['url' => ['foto/cropandsave',  Auth::user()->perfil->id ], 'files' => true, 'data-redirect' => '/quiz/contemais', 'id'=>'quiz_foto_form', 'data-loading'=>'form-loading']) !!}
+                    
+                    <h2 class="text-center">{{ trans("global.quiz_profile_customize") }}</h2>
+                    <label class="btn btn-acao btn-upload margin-b-1" for="input-quiz-foto-perfil" title="Upload image file">
+                        <input class="sr-only" id="input-quiz-foto-perfil" name="file" accept="image/*" type="file">
+                        <span data-original-title="Import image with Blob URLs" class="docs-tooltip" data-toggle="tooltip" title="">
+                            <span class="fa fa-upload"></span>
+                            SELECIONE UMA IMAGEM
+                        </span>
+                    </label>    
+                    <div id="cropper-quiz">
+                        <img src="{{ $foto?$foto:'/img/interrogacao.png' }}" />
+                    </div>
+                    {!! Form::hidden("tipoEntidade",  "App\Perfil") !!}
+                    {!! Form::hidden("x",  0, ['id' => 'x-quiz-perfil']) !!}
+                    {!! Form::hidden("y",  0, ['id' => 'y-quiz-perfil']) !!}
+                    {!! Form::hidden("w",  0, ['id' => 'w-quiz-perfil']) !!}
+                    {!! Form::hidden("h",  0, ['id' => 'h-quiz-perfil']) !!}
+                    {!! Form::hidden("r",  0, ['id' => 'r-quiz-perfil']) !!}
+                    {!! Form::submit( "cortar foto", ['class' => 'btn btn-primario btn-acao']) !!}
+                    <i id="form-loading" class="fa fa-spinner fa-pulse fa-2x " style="display:none"></i>
+
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
