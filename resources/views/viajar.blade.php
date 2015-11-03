337529804d87a92d46d714b2dfb7d199dd4b864e
Evandro Carreira <evandro.carreira@gmail.com>
2015-11-03 17:18:55 -0200

    Aviso de indisponibilidade em resoluções pequenas

--- resources/views/viajar.blade.php
+++ resources/views/viajar.blade.php
@@ -1,6 +1,29 @@
 @extends('app')
 
 @section('pilar')
+
+        <div class="fundo-cheio text-justified padding-b-2 padding-t-2 hidden-md hidden-lg ">
+            <div class="row">
+                <h3 class="font-bold-upper text-center margin-b-2">
+                    {{ trans('global.crowdfunding_hi_there') }}
+                </h3>
+            </div>
+            <div class="row margin-b-10">
+                <h4 class="col-xs-12 text-center">
+                    {{ trans('global.crowdfunding_page_and_plan') }}
+                </h4>
+                <h4 class="col-xs-12 text-center margin-b-1">
+                    {{ trans('global.crowdfunding_we_need_you') }}
+                </h4>
+                <h4 class="col-xs-12 margin-t-2 text-center">
+                    {{ trans('global.crowdfunding_support_vivala') }}
+                </h4>
+                <h4 class="col-xs-12 text-center">
+                    <a href="http://www.catarse.me/pt/vivala" alt="{{ trans('global.crowdfunding_a_alt_crowdvivala') }}" title="{{ trans('global.crowdfunding_a_title_crowdvivala') }}" class="laranja" target="_blank">www.catarse.me/pt/vivala</a>
+                </h4>
+            </div>
+        </div>
+
 	<nav class="navbar navbar-default menu-top header">
 		@include('header')
 	</nav>
