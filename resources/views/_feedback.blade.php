4228d13a67fe993b54c6a0baa0b55462aa7f22fc
Bruno Luiz <brunoluizgr@gmail.com>
2015-11-11 01:58:06 -0200

    minorfix UX formFeedback

--- resources/views/_feedback.blade.php
+++ resources/views/_feedback.blade.php
@@ -2,7 +2,10 @@
     <div class="modal-dialog modal-md">
         <div class="modal-content">
             <div class="modal-body">
-                <h3 class="texto-preto">{{ trans('global.lbl_feedback_give_yours') }}</h3>
+                <div class="row">
+                    <div class="col-sm-10"><h3 class="texto-preto">{{ trans('global.lbl_feedback_give_yours') }}</h3></div>
+                    <div class="col-sm-2"><button type="button" class="btn" data-dismiss="modal"><i class="fa fa-times"></i></button></div>
+                </div>
                 {!! Form::open(['url' => '/paginas/feedback', 'method' => 'POST', 'id' => 'form-feedback', 'data-callback' => 'retornoFormFeedback', 'data-loading'=>'form-loading']) !!}
 
                 {!! Form::select("tipo",
@@ -18,7 +21,6 @@
 
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">{{trans('global.lbl_close')}}</button>
                 {!! Form::submit(trans('global.lbl_submit'), ['class' => 'btn btn-primario btn-acao pull-right']) !!}
                 <i id="form-loading" class="fa fa-spinner fa-pulse fa-2x margin-t-1" style="display:none"></i>
                 {!! Form::close() !!}
