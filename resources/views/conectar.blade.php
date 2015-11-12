3ab0ba8dd2d7be25d6e824da61a1838a67d51368
Evandro Carreira <evandro.carreira@gmail.com>
2015-11-12 12:33:18 -0200

    Hide de tudo no mobile

--- resources/views/conectar.blade.php
+++ resources/views/conectar.blade.php
@@ -2,7 +2,7 @@
 
 @section('pilar')
 
-        <div class="fundo-cheio text-justified padding-b-2 padding-t-2 hidden-md hidden-lg ">
+        <div class="mobile-warning fundo-cheio text-justified padding-b-2 padding-t-2 hidden-md hidden-lg ">
             <div class="row">
                 <h3 class="font-bold-upper text-center margin-b-2">
                     {{ trans('global.mobile_warning_welcome') }}
@@ -10,9 +10,6 @@
                 </h3>
             </div>
             <div class="row margin-b-10">
-                <h4 class="col-xs-12 text-center">
-                    {{ trans('global.mobile_warning_welcome') }}
-                </h4>
                 <h4 class="col-xs-12 text-center margin-b-1">
                     {{ trans('global.mobile_warning_desctopo') }}
                 </h4>
@@ -25,25 +22,25 @@
             </div>
         </div>
 
-	<nav class="navbar navbar-default menu-top header">
+	<nav class="navbar navbar-default menu-top header hidden-xs">
 		@include('header')
 	</nav>
 	{{-- Seção do topo com infos (a principio só do usuario) --}}
 	@yield('barra-topo')
 
-	<nav class="col-xs-12 col-sm-3 col-md-2 left-panel">
+	<nav class="col-xs-12 col-sm-3 col-md-2 left-panel hidden-xs">
 		@include('conectar.menulateral')
 	</nav>
 
-	<nav class="col-xs-12 col-sm-6 col-md-8">
+	<nav class="col-xs-12 col-sm-6 col-md-8 hidden-xs">
 		@yield('content')
 	</nav>
 
-	<nav class="hidden-xs col-sm-3 col-md-2 right-panel">
+	<nav class="hidden-xs col-sm-3 col-md-2 right-panel hidden-xs">
 		@include('conectar._sugestoes')
 	</nav>
 
-	<footer class="footer">
+	<footer class="footer hidden-xs">
 	@include('footer')
 	</footer>
 
