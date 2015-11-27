a36fdb3a6ad0db58decd8808dd2f4218c728a0c4
Renato Gomes <renatoo.agomes@gmail.com>
2015-11-26 22:31:16 -0200

    commit com links de roteiros para a blackfriday

--- resources/views/viajar/sugestoespacotes.blade.php
+++ resources/views/viajar/sugestoespacotes.blade.php
@@ -1,53 +1,88 @@
 <h4 class="suave">{{ trans('global.wannatravel_package_find_best_destinies') }}</h4>
 <ul class="sugestoes sugestoes-pacotes">
     <li>
-        <a href="#" class="click-img-no-border ativa-modal-quimera" data-url="http://www.e-agencias.com.br/vivala/cp/search/SAO/RIO/2016-08-10/2016-08-17/1/0/0/NA/2016-08-10/2016-08-17/1">
+        <a href="#" class="click-img-no-border ativa-modal-quimera" data-url="https://www.e-agencias.com.br/vivala/flights/search/OneWay/SAO/RIO/2016-03-11/1/0/0/">
             <div class="round foto">
                 <div class="cover-explode">
                     <img src="/img/dummyrio.png">
                 </div>
             </div>
-            <strong class="col-sm-12">Rio de Janeiro</strong>
-            <span class="col-sm-12">{{ trans('global.dummy_travel_1') }}</span>
+            <strong class="col-sm-12">Black Friday</strong>
+            <span class="col-sm-12">{{ trans('global.blackfriday-1') }}</span>
             <button class="suave" type="button" >{{ trans('global.lbl_seemore') }}</button>
         </a>
     </li>
     <li>
-        <a href="#" class="click-img-no-border ativa-modal-quimera" data-url="https://www.e-agencias.com.br/vivala/flights/search/RoundTrip/SAO/FOR/2016-01-18/2016-01-22/1/0/0/">
+        <a href="#" class="click-img-no-border ativa-modal-quimera" data-url="https://www.e-agencias.com.br/vivala/flights/search/RoundTrip/SAO/MCZ/2016-04-22/2016-04-27/1/0/0">
             <div class="round foto">
                 <div class="cover-explode">
-                    <img src="/img/dummyfortaleza.png">
+                    <img src="/img/dummyrio.png">
                 </div>
             </div>
-            <strong class="col-sm-12">Fortaleza</strong>
-            <span class="col-sm-12">{{ trans('global.dummy_travel_2') }}</span>
-            <button class="suave" type="button">{{ trans('global.lbl_seemore') }}</button>
+            <strong class="col-sm-12">Black Friday</strong>
+            <span class="col-sm-12">{{ trans('global.blackfriday-2') }}</span>
+            <button class="suave" type="button" >{{ trans('global.lbl_seemore') }}</button>
         </a>
     </li>
-    {{--
     <li>
-        <a href="#" class="click-img-no-border ativa-modal-quimera" data-url="https://www.e-agencias.com.br/vivala/hotels/details/FLN/2016-02-05/2016-02-10/1/777287/BRL">
+        <a href="#" class="click-img-no-border ativa-modal-quimera" data-url="https://www.e-agencias.com.br/vivala/flights/search/OneWay/RIO/SAO/2016-01-28/1/0/0/">
             <div class="round foto">
                 <div class="cover-explode">
-                    <img src="/img/dummyrecife.png">
+                    <img src="/img/dummyrio.png">
                 </div>
             </div>
-            <strong class="col-sm-12">Recife</strong>
-            <span class="col-sm-12">{{ trans('global.dummy_travel_3') }}</span>
-            <button class="suave" type="button">{{ trans('global.lbl_seemore') }}</button>
+            <strong class="col-sm-12">Black Friday</strong>
+            <span class="col-sm-12">{{ trans('global.blackfriday-3') }}</span>
+            <button class="suave" type="button" >{{ trans('global.lbl_seemore') }}</button>
         </a>
     </li>
-    --}}
     <li>
-        <a href="#" class="click-img-no-border hidden-xs ativa-modal-quimera" data-url="https://www.e-agencias.com.br/vivala/hotels/search/MV1/2015-11-27/2015-11-29/2">
+        <a href="#" class="click-img-no-border ativa-modal-quimera" data-url="https://www.e-agencias.com.br/vivala/hotels/search/FLN/2016-01-18/2016-01-21/1">
             <div class="round foto">
                 <div class="cover-explode">
-                    <img src="/img/dummymonteverde.png">
+                    <img src="/img/dummyrio.png">
                 </div>
             </div>
-            <strong class="col-sm-12">Monte Verde</strong>
-            <span class="col-sm-12">{{ trans('global.dummy_travel_4') }}</span>
-            <button class="suave" type="button">{{ trans('global.lbl_seemore') }}</button>
+            <strong class="col-sm-12">Black Friday</strong>
+            <span class="col-sm-12">{{ trans('global.blackfriday-4') }}</span>
+            <button class="suave" type="button" >{{ trans('global.lbl_seemore') }}</button>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="click-img-no-border ativa-modal-quimera" data-url="https://www.e-agencias.com.br/vivala/hotels/search/AC1/2015-12-15/2015-12-18/2">
+            <div class="round foto">
+                <div class="cover-explode">
+                    <img src="/img/dummyrio.png">
+                </div>
+            </div>
+            <strong class="col-sm-12">Black Friday</strong>
+            <span class="col-sm-12">{{ trans('global.blackfriday-5') }}</span>
+            <button class="suave" type="button" >{{ trans('global.lbl_seemore') }}</button>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="click-img-no-border ativa-modal-quimera" data-url="https://www.e-agencias.com.br/vivala/hotels/search/MO1/2016-01-18/2016-01-21/1">
+            <div class="round foto">
+                <div class="cover-explode">
+                    <img src="/img/dummyrio.png">
+                </div>
+            </div>
+            <strong class="col-sm-12">Black Friday</strong>
+            <span class="col-sm-12">{{ trans('global.blackfriday-6') }}</span>
+            <button class="suave" type="button" >{{ trans('global.lbl_seemore') }}</button>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="click-img-no-border ativa-modal-quimera" data-url="https://www.e-agencias.com.br/vivala/packages/items/SAO/NAT/2016-03/NA/2">
+            <div class="round foto">
+                <div class="cover-explode">
+                    <img src="/img/dummyrio.png">
+                </div>
+            </div>
+            <strong class="col-sm-12">Black Friday</strong>
+            <span class="col-sm-12">{{ trans('global.blackfriday-7') }}</span>
+            <button class="suave" type="button" >{{ trans('global.lbl_seemore') }}</button>
         </a>
     </li>
 </ul>
+
