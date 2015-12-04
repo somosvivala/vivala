1d55036ba8a856daeb059be4c238e566b0663405
Zord <zord@vivalabrasil.com.br>
2015-12-04 09:20:32 -0500

    blades de assinatura

---
+++ resources/views/paginas/assinaturas/assinaturadan.blade.php
@@ -0,0 +1,81 @@
+<table width="550" style="border:2px solid #f16f2b; color: #f16f2b; font-family: Helvetica, Arial, Verdana, sans-serif; padding: 1em;">
+    <tr>
+        <td style="border-right:2px solid #f16f2b;">
+            <a href="http://www.vivala.com.br" style="color: #f16f2b;margin-right:1em;text-decoration:none;">
+                <img src="http://dev.vivalabrasil.com.br/logo.png" width="150px" style="margin: 0px;padding:2em 0em 2em 0;">
+            </a>
+        </td>
+        <td>
+            <table>
+                <tr>
+                    <td>
+                        <table>
+                            <tr>
+                                <td width="100" style="width:100px;">
+                                    <img align="top" style="float:left" src="http://dev.vivalabrasil.com.br/email/fotodan.png" alt="">
+                                </td>
+                                <td>
+                                    <div style="color: #f16f2b;text-decoration:none;">
+                                        Fundador e Presidente
+                                        <h2 style="color: #f16f2b;">DANIEL CABRERA</h2>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table style="width:100%;">
+                            <tr style="width:100%;">
+                                <td width="280" style="width:280px">
+                                    <a href="http://www.vivala.com.br" style="color: #f16f2b;text-decoration:none;">
+                                        <img
+                                        src="http://dev.vivalabrasil.com.br/email/iconmundo.png"
+                                        alt="Site:" align="top" style="vertical-align:top;margin-top:0.2em">
+                                        www.vivala.com.br
+                                    </a>
+                                </td>
+                                <td style="float:right">
+                                    <a href="http://facebook.com/somosvivala" style="color: #f16f2b;text-decoration:none;">
+                                        <img src="http://dev.vivalabrasil.com.br/email/iconfb.png" alt="Facebook:" align="top" style="vertical-align:top;margin-top:0.2em">
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="tel: +55 11 982 041 821" style="color: #f16f2b;text-decoration:none;">
+                                        <img src="http://dev.vivalabrasil.com.br/email/icontel.png"
+                                        alt="Telefone:" align="top" style="vertical-align:top;margin-top:0em">
+                                        +55 11 982 041 821
+                                    </a>
+                                </td>
+                                <td style="float:right">
+                                    <a href="http://instagram.com/somosvivala" style="color: #f16f2b;text-decoration:none;">
+                                        <img src="http://dev.vivalabrasil.com.br/email/iconinsta.png" alt="Instagram:" align="top" style="vertical-align:top;margin-top:0.2em">
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="mailto:daniel.cabrera@vivala.com.br" style="color: #f16f2b;text-decoration:none;">
+                                        <img src="http://dev.vivalabrasil.com.br/email/iconemail.png" alt="Email:" align="top" style="vertical-align:top;margin-top:0.2em">
+                                        daniel.cabrera@vivala.com.br
+                                    </a>
+                                </td>
+                                <td style="float:right">
+                                    <a href="http://linkedin.com/company/vival%C3%A1" style="color: #f16f2b;text-decoration:none;">
+                                        <img src="http://dev.vivalabrasil.com.br/email/iconlinkedin.png"
+                                        alt="Linkedin:" align="top" style="vertical-align:top;margin-top:0.2em">
+                                    </a>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>            
+                </tr>
+
+            </table>
+        </td>
+    </tr>
+</table>
+
