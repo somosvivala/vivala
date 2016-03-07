3dc3e13fc105507e8f5dc4c20a5992e3cbdfcc79
brunoluizgr <brunoluizgr@gmail.com>
2016-03-07 15:53:25 -0300

    Atualizado email de cancelamento.

--- resources/views/emails/clickbus/cancelamento.blade.php
+++ resources/views/emails/clickbus/cancelamento.blade.php
@@ -1,2 +1,261 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-micros oft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<html>
+  <head>
+    <!--[if gte mso 15]>
+    <xml>
+    <o:OfficeDocumentSettings>
+    <o:AllowPNG/>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+    </o:OfficeDocumentSettings>
+    </xml>
+    <![endif]-->
+    <meta content="text/html">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial -scale=1">
+  </head>
+  <body>
+    <div>
+      <table style="border-radius:15px" cellpadding="0" cellspacing="0" width="100%">
+        <tbody>
+          <tr>
+            <td>
+              <table style="border-radius:15px;border:solid thin #999999" cellpadding="10px" cellspacing="0" align="center" bgcolor="#ffffff" width="600">
+                <tbody>
+                  <tr>
+                    <td style="border-top-left-radius:15px;border-top-right-radius:15px" bgcolor="#ffffff">
+                      <table style="border-radius:15px" cellpadding="0" cellspacing="0" width="100%">
+                        <tbody>
+                          <tr>
+                            <td valign="top" width="15%">
+                              <a href="http://www.clickbus.com.br/?utm_source=transactional&amp;utm_medium=crm&amp;utm_campaign=success_email&amp;utm_content=header_logoClickBus" target="_blank"><img src="http://static2.clickbus.com.br/EmailMarketing/2014/new/logo.png" alt="" style="border:0px" height="53" width="156">
+                              </a>
+                            </td>
+
+                            <td style="text-align:right;padding-right:20px;font-size:18px" width="60%">
+                              <table align="right" width="100%">
+                                <tbody>
+                                  <tr>
+
+                                    <td style="text-align:right;font-size:18px;padding-right:5px;font-family:Arial">
+                                      <a href="https://app.adjust.io/dli4ab_n6lfvl?fallback=http://www.clickbus.com.br/pt/mobile" style="text-decoration:none;color:#000" target="_blank">Baixe nosso App!</a>
+                                    </td>
+
+                                    <td style="text-align:center;font-size:18px" width="6%">
+                                      <a href="https://app.adjust.io/n6lfvl" target="_blank">
+                                        <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailMarketing/2015/novo/CRM_Header-App_1.png">
+                                      </a>
+                                    </td>
+
+                                    <td style="text-align:center;font-size:18px" width="6%">
+                                      <a href="https://app.adjust.io/dli4ab" target="_blank">
+                                        <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailMarketing/2015/novo/CRM_Header-App_2.png">
+                                      </a>
+                                    </td>
+
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td bgcolor="#474747">
+                      <table style="padding:10px 40px 10px 40px" cellpadding="10" cellspacing="0" width="100%">
+                        <tbody>
+                          <tr>
+                            <td style="padding:10px 0px 5px 0px;color:#fff;font-family:'Roboto',sans-serif;font-weight:100;font-size:26px;border-top:solid thin #fff;border-bottom:solid thin #fff" align="center">
+                              <p>Sua Compra foi Cancelada!</p>
+                              <p style="font-weight:100;font-size:15px">
+                              Prezado(a) <span style="font-weight:400">NOME DO PASSAGEIRO</span>, seu pagamento não foi concluí­do.<br>
+                              Para realizar um novo pedido entre em nosso site:<br>
+                              <a style="text-decoration:none" href="https://www.clickbus.com.br/?utm_source=transactional&amp;utm_medium=crm&amp;utm_campaign=cancelation_email&amp;content=link_site" target="_blank"><font color="white" size="5">www.clickbus.com.br</font></a>
+                              </p>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td bgcolor="#fff">
+                      <table style="border-bottom-left-radius:5px;border-bottom-right-radius:5px;border-bottom:solid thin #999999;border-left:solid thin #999999;border-top:solid thin #999999;border-right:solid thin #999999;font-family:'Arial',sans-serif;font-weight:300;font-size:16px;color:#000" cellpadding="5px" cellspacing="0" align="center" bgcolor="#F4F4F4" width="90%">
+                        <tbody>
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Rota
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px;color:#000">
+                              Sao Paulo, SP - Tiete - Rio de Janeiro, RJ - Novo Rio
+                            </td>
+                          </tr>
+
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Embarque:
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px;color:#000">
+                              <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailTransational/calendario.png" alt="Calend" style="display:inline" height="14" width="13">
+                              &nbsp;15/05/2013 -
+                              <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailTransational/relogsmall.png" alt="Relog" style="display:inline" border="0" height="14" width="14">
+                              &nbsp; 16:00
+                            </td>
+                          </tr>
+
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                              <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                                Desembarque:
+                              </td>
+                              <td style="text-align:right;padding-right:10px;font-size:12px">
+                                <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailTransational/calendario.png" alt="Calend" style="display:inline" height="14" width="13">
+                                &nbsp;16/05/2013 -
+                                <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailTransational/relogsmall.png" alt="Relog" style="display:inline" border="0" height="14" width="14">
+                                &nbsp; 16:00
+                              </td>
+                            </tr>
+
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Nome do passageiro:
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px">
+                              NOME DO PASSAGEIRO
+                            </td>
+                          </tr>
+
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Documento:
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px">
+                              NUMERO DO DOCUMENTO
+                            </td>
+                          </tr>
+
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Poltrona:
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px">
+                              NUMERO DA POLTRONA
+                            </td>
+                          </tr>
+
+                        </tbody>
+                      </table>
+
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td bgcolor="#fff">
+                      <table style="border-bottom-left-radius:5px;border-bottom-right-radius:5px;border-bottom:solid thin #999999;border-left:solid thin #999999;border-top:solid thin #999999;border-right:solid thin #999999;font-family:'Arial',sans-serif;font-weight:300;font-size:16px;color:#000" cellpadding="5px" cellspacing="0" align="center" bgcolor="#F4F4F4" width="90%">
+                        <tbody>
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Rota
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px;color:#000">
+                              Rio de Janeiro, RJ - Novo Rio - Sao Paulo, SP - Tiete
+                            </td>
+                          </tr>
+
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Embarque:
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px;color:#000">
+                              <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailTransational/calendario.png" alt="Calend" style="display:inline" height="14" width="13">
+                              &nbsp;19/05/2013 -
+                              <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailTransational/relogsmall.png" alt="Relog" style="display:inline" border="0" height="14" width="14">
+                              &nbsp; 16:00
+                            </td>
+                          </tr>
+
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Desembarque:
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px">
+                              <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailTransational/calendario.png" alt="Calend" style="display:inline" height="14" width="13">
+                              &nbsp;19/05/2013 -
+                              <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailTransational/relogsmall.png" alt="Relog" style="display:inline" border="0" height="14" width="14">
+                              &nbsp; 16:00
+                            </td>
+                          </tr>
+
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Nome do passageiro:
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px">
+                              NOME DO PASSAGEIRO
+                            </td>
+                          </tr>
+
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Documento:
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px">
+                              NUMERO DOCUMENTO
+                            </td>
+                          </tr>
+
+                          <tr style="height:35px;font-size:14px;font-weight:100" bgcolor="#F4F4F4">
+                            <td style="text-align:left;padding-left:10px;color:#000;font-family:Arial">
+                              Poltrona:
+                            </td>
+                            <td style="text-align:right;padding-right:10px;font-size:12px">
+                              NUMERO DA POLTRONA
+                            </td>
+                          </tr>
+
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td style="padding:0px 0px 0px 0px;font-family:'Arial',sans-serif;font-weight:300;font-size:18px;text-align:center;color:#484848;height:5px;border-top:2px solid #ddd">
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>
+                      <p style="font-size:14px;font-family:Arial" align="center">
+                        Qualquer dÃºvida ligue para o <strong>SAC</strong>: 0800-038-6013.
+                      </p>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td style="border-bottom:1px solid #ddd;border-top:1px solid #ddd;padding-top:5px;padding-bottom:5px;font-size:12px;font-family:Arial" align="center" width="90%">
+                      Av. QueirÃ³z Filho, 1700, Bloco C, Sala 904. CEP 05319-000 SÃ£o Paulo, SP- Brasil.
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td style="padding:5px 0px 0px 0px" align="center">
+                      <img src="https://s3-sa-east-1.amazonaws.com/static2.clickbus.com.br/EmailMarketing/2016/copyright_2016.jpg">
+                    </td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <table cellpadding="10px" cellspacing="0" align="center" bgcolor="#ffffff" width="600">
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
+</html>
