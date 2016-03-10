f86e406638f01f4480069417d681eab941147829
brunoluizgr <brunoluizgr@gmail.com>
2016-03-10 16:31:37 -0300

    [IntroTour] Fix do Tour aparecendo na Home.

--- resources/assets/js/tour.js
+++ resources/assets/js/tour.js
@@ -409,11 +409,7 @@ $(document).ready(function() {
 
 // ##### INTERNO
 // Se a variável for FALSE (setada pelo user), posso iniciar este Tour
-  if((!parseInt(ligaIntroTour))){
-
-  /* Comentado pois não estava funcionando na versão da HOME -> VIAJAR
-  && ($("#tour-pilares").hasClass("pilar-home"))){
-  */
+  if((!parseInt(ligaIntroTour)) && ($("#tour-pilares").hasClass("pilar-viajar"))){
     //console.log("Estou dentro da Vivalá, preciso mostrar a TOUR INICIAL");
     // TOUR INTERNO
     switch(linguaAtiva){
