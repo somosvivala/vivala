bb8cb153e9a6d807bdd9ac3af3d515c1f86117ce
Renato Gomes <renatoo.agomes@gmail.com>
2016-03-21 17:57:51 -0300

    fix use CompraClickbusPoltrona

--- app/Http/Controllers/ClickBusController.php
+++ app/Http/Controllers/ClickBusController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\ClickBusPlace;
 use App\CompraClickbus;
+use App\CompraClickbusPoltrona;
 use App\ClickBusCompany;
 use Input;
 use App\Repositories\ClickBusRepository;
