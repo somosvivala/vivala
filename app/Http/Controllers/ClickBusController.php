cb52553f0ea9ccf2b915b532aae001ae63588628
Renato Gomes <renatoo.agomes@gmail.com>
2016-03-21 16:06:48 -0300

    use ClickBusCompany no Controller

--- app/Http/Controllers/ClickBusController.php
+++ app/Http/Controllers/ClickBusController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\ClickBusPlace;
 use App\CompraClickbus;
+use App\ClickBusCompany;
 use Input;
 use App\Repositories\ClickBusRepository;
 use App\Http\Requests\SelecionarPoltronasClickbusRequest;
