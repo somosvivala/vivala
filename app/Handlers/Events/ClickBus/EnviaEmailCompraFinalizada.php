0db09f573bba8488363aaf29f427452671b487d9
Renato Gomes <renatoo.agomes@gmail.com>
2016-05-02 16:23:55 -0300

    Commit disparando email pra vivalá quando a compra é finalizada com sucesso

--- app/Handlers/Events/ClickBus/EnviaEmailCompraFinalizada.php
+++ app/Handlers/Events/ClickBus/EnviaEmailCompraFinalizada.php
@@ -38,6 +38,13 @@ class EnviaEmailCompraFinalizada {
                 $message->from('noreply@vivalabrasil.com.br', 'Vivalá');
             });
 
+            //enviando email para a vivalá avisando que essa compra foi finalizada
+            Mail::send('emails.clickbus.sucesso', ['Compra' => $Compra], function ($message) use ($Compra) {
+                $message->to("contato@vivalabrasil.com.br", "Vendas")->subject("Alguem comprou passagem rodoviaria na Vivalá");
+                $message->from('noreply@vivalabrasil.com.br', 'Vivalá');
+            });
+
+
         } else {
             //Envia email de pagamento pendente
             Mail::send('emails.clickbus.pendente', ['Compra' => $Compra], function ($message) use ($Compra) {
--- app/Handlers/Events/ClickBus/EnviaEmailPagamentoConfirmado.php
+++ app/Handlers/Events/ClickBus/EnviaEmailPagamentoConfirmado.php
@@ -32,6 +32,13 @@ class EnviaEmailPagamentoConfirmado {
             $message->to($Compra->buyer_email, $Compra->buyer_firstname)->subject(trans('clickbus.clickbus_email-vivala-subject-success'));
             $message->from('noreply@vivalabrasil.com.br', 'Vivalá');
       });
+
+        //enviando email para a vivalá avisando que essa compra foi finalizada
+        Mail::send('emails.clickbus.sucesso', ['Compra' => $Compra], function ($message) use ($Compra) {
+            $message->to("contato@vivalabrasil.com.br", "Vendas")->subject("Alguem comprou passagem rodoviaria na Vivalá");
+            $message->from('noreply@vivalabrasil.com.br', 'Vivalá');
+        });
+
 	}
 
 }
