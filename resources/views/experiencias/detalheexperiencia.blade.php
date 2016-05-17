71f493f5ec19dd4bd70074188dda36fe413e7d9a
Evandro Carreira <evandro.carreira@gmail.com>
2016-05-17 11:39:05 -0300

    Css de lista de experiências

--- resources/views/experiencias/detalheexperiencia.blade.php
+++ resources/views/experiencias/detalheexperiencia.blade.php
@@ -1,12 +1,18 @@
 @extends(Auth::user() ? 'mobilelogado' : 'mobiledeslogado')
 
 @section('content')
-<div class="text-center row">
-    {{-- <img class="col-sm-6" src="{{ $Experiencia->foto }}" alt="{{ $Experiencia->titulo }}"> --}}
-    <img class="col-sm-6" src="https://dev.vivala.com.br/img/dummyvoos.jpg">
-</div>
-<span class="col-sm-6">{{ $Experiencia->descricao }}</span>
-<span class="col-sm-6">{{ $Experiencia->preco }}</span>
-<span class="col-sm-6">{{ $Experiencia->preco }}</span>
-<a class="btn" href="/experiencias/checkout/{{ $Experiencia->id }}">Bookar</a>
+<section class="experiencia">
+    <div class="foto-experiencia margin-t-1">
+        <div class="foto-img" style="background-image:url('https://dev.vivala.com.br/img/dummyvoos.jpg')">
+            <div class="categorias">
+                <i class="fa fa-paw"></i>
+            </div>
+        </div>
+    </div>
+
+    <span class="col-sm-6">{{ $Experiencia->descricao }}</span>
+    <span class="col-sm-6">{{ $Experiencia->preco }}</span>
+    <span class="col-sm-6">{{ $Experiencia->preco }}</span>
+    <a class="btn" href="/experiencias/checkout/{{ $Experiencia->id }}">Bookar</a>
+</section >
 @endsection
--- resources/views/experiencias/listaexperiencias.blade.php
+++ resources/views/experiencias/listaexperiencias.blade.php
@@ -8,14 +8,20 @@
     @foreach($experiencias as $k=>$Experiencia)
     <li class="row">
         <a href="/experiencias/{{ $Experiencia->id}}">
-            <div class="col-xs-6 {{ $k%2==0?'pull-right':'pull-left'}} foto" style="background-image:url('https://dev.vivala.com.br/img/dummyvoos.jpg')">
-                <i class="fa fa-paw categoria"></i>
+            <div class="{{ $k%2==0?'direita':'esquerda'}} foto">
+                <div class="foto-img" style="background-image:url('https://dev.vivala.com.br/img/dummyvoos.jpg')">
+                    <div class="categorias">
+                        <i class="fa fa-paw"></i>
+                    </div>
+                </div>
                 {{-- <img class="col-sm-6" src="{{ $Experiencia->foto }}" alt="{{ $Experiencia->titulo }}"> --}}
             </div>
-            <div class="descricao col-xs-6">
-                <span class="col-xs-12 local"><i class="fa fa-map-marker"></i> Sao Paulo</span>
-                <span class="col-xs-12">{{ $Experiencia->descricao }}</span>
-                <span class="col-xs-12 preco">R${{ $Experiencia->preco }}</span>
+            <div class="{{ $k%2!=0?'direita':'esquerda'}} descricao">
+                <div class="container">
+                    <span class="col-xs-12 local"><i class="fa fa-map-marker"></i> Sao Paulo</span>
+                    <span class="col-xs-12">{{ $Experiencia->descricao }}</span>
+                    <span class="col-xs-12 preco">R${{ $Experiencia->preco }}</span>
+                </div>
             </div>
         </a>
     </li>
