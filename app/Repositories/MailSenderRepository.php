9824dd26e7cd248e668ffe841256fb586502deb8
brunoluizgr <brunoluizgr@gmail.com>
2016-06-03 18:58:08 -0300

    mudança no email sandbox para teste@ dentro do repositorio, facilita a checagem de testes.

--- app/Repositories/MailSenderRepository.php
+++ app/Repositories/MailSenderRepository.php
@@ -13,7 +13,7 @@ class MailSenderRepository {
       if(app()->environment('production'))
         $message->to('contato@vivalabrasil.com.br', 'Vivalá')->subject('[COTAÇÃO DE VIAGEM] Enviado pela Plataforma');
       else if(app()->environment('development'))
-        $message->to('bruno.luiz@vivalabrasil.com.br', 'Vivalá')->subject('[SANDBOX - COTAÇÃO DE VIAGEM] Enviado pela Plataforma');
+        $message->to('teste@vivalabrasil.com.br', 'Vivalá')->subject('[SANDBOX - COTAÇÃO DE VIAGEM] Enviado pela Plataforma');
       $message->from('noreply@vivalabrasil.com.br', 'Vivalá');
     });
   }
