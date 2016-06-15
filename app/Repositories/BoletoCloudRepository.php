d9ac458068f33678bf033b06ab2abf075f326e00
Renato Gomes <renatoo.agomes@gmail.com>
2016-06-15 19:10:25 -0300

    commit com rota para teste de geracao de boleto e BoletoCloudRepository com o conteudo da requisicao dummy

---
+++ app/Repositories/BoletoCloudRepository.php
@@ -0,0 +1,180 @@
+<?php namespace App\Repositories;
+
+/**
+ * Classe para centralizar metodos referentes aos boletos da BoletoCloud
+ */
+class BoletoCloudRepository
+{
+    /**
+     * Construtor obtendo informacoes necessarias do env
+     */
+    function __construct()
+    {
+        //pegar informacoes sensiveis no enviroment (agencia, conta, etc..)
+    }
+
+
+    /**
+     * Metodo para gerar um boleto
+     */
+    public function gerarBoleto()
+    {
+        //logica de geracao de boleto
+    }
+
+
+
+    /**
+     * Metodo para testar a geracao de um boleto no sandbox
+     * @param $numeroSequencial - numero sequecial utilizado para identificar o boleto
+     */
+    public function gerarBoletoTeste($numeroSequencial)
+    {
+
+        /*
+         * Exemplo de criação de um boleto utilizando o método HTTP POST. e exibe o conteúdo PDF retornado no navegador.
+         * Após obter o PDF do boleto o arquivo é enviado como resposta para visualização no navegador.
+         *
+         * cURL - http://php.net/manual/pt_BR/book.curl.php
+         **/
+
+        #Dados do boleto
+
+        $fields = array(
+            'boleto.conta.banco'=> '237',
+            'boleto.conta.agencia'=> '1234-5',
+            'boleto.conta.numero'=> '123456-0',
+            'boleto.conta.carteira'=> '12',
+            'boleto.beneficiario.nome'=> 'DevAware Solutions',
+            'boleto.beneficiario.cprf'=> '15.719.277/0001-46',
+            'boleto.beneficiario.endereco.cep'=> '59020-000',
+            'boleto.beneficiario.endereco.uf'=> 'RN',
+            'boleto.beneficiario.endereco.localidade'=> 'Natal',
+            'boleto.beneficiario.endereco.bairro'=> 'Petrópolis',
+            'boleto.beneficiario.endereco.logradouro'=> 'Avenida Hermes da Fonseca',
+            'boleto.beneficiario.endereco.numero'=> '384',
+            'boleto.beneficiario.endereco.complemento'=> 'Sala 2A, segundo andar',
+            'boleto.emissao'=> '2014-07-11',
+            'boleto.vencimento'=> '2020-05-30',
+            'boleto.documento'=> 'EX1',
+            'boleto.sequencial'=> $numeroSequencial,
+            'boleto.titulo'=> 'DM',
+            'boleto.valor'=> '1250.43',
+            'boleto.pagador.nome'=> 'Alberto Santos Dumont',
+            'boleto.pagador.cprf'=> '111.111.111-11',
+            'boleto.pagador.endereco.cep'=> '36240-000',
+            'boleto.pagador.endereco.uf'=> 'MG',
+            'boleto.pagador.endereco.localidade'=> 'Santos Dumont',
+            'boleto.pagador.endereco.bairro'=> 'Casa Natal',
+            'boleto.pagador.endereco.logradouro'=> 'BR-499',
+            'boleto.pagador.endereco.numero'=> 's/n',
+            'boleto.pagador.endereco.complemento'=> 'Sítio - Subindo a serra da Mantiqueira',
+            'boleto.instrucao'=> array(
+                'Atenção! NÃO RECEBER ESTE BOLETO.',
+                'Este é apenas um teste utilizando a API Boleto Cloud',
+                'Mais info em http://www.boletocloud.com/app/dev/api'
+            )
+        );
+
+        #Aplicando formato de formulário
+
+        $fields_string = '';
+
+        foreach($fields as $key=>$value) {
+            if(is_array($value)){
+                foreach($value as $v){
+                    $fields_string .= urlencode($key).'='.urlencode($v).'&';
+                }
+            }else{
+                $fields_string .= urlencode($key).'='.urlencode($value).'&';	
+            }
+        }
+
+        $data = rtrim($fields_string, '&');
+
+        #Definindo conteúdo da requisição e tipos de respostas aceitas
+
+        #Pode responder com o boleto ou mensagem de erro
+        $accept_header = 'Accept: application/pdf, application/json';
+
+        #Estou enviando esse formato de dados
+        $content_type_header = 'Content-Type: application/x-www-form-urlencoded; charset=utf-8';
+
+        $headers = array($accept_header, $content_type_header);
+
+        #Configurações do envio
+
+        $url = 'https://sandbox.boletocloud.com/api/v1/boletos';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true); 
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_USERPWD, "api-key_Apa_-4G10rXU80t7eM_tCEzAnqxqwj9La6Er8NgvHA0=:token"); #API TOKEN
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);# Basic Authorization
+        curl_setopt($ch, CURLOPT_HEADER, true);#Define que os headers estarão na resposta
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); #Para uso com https
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); #Para uso com https
+
+        #Envio
+
+        $response = curl_exec($ch);
+
+        #Principais meta-dados da resposta
+
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+
+        #Fechar processo de comunicação
+        curl_close($ch);
+
+        #Processando a resposta
+
+        $created = 201; #Constante que indica recurso criado (Boleto Criado)
+
+        #Separando header e body na resposta
+
+        $header = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
+        $header_array = explode("\r\n", $header);
+
+        #Principais headers
+        $boleto_cloud_version = '';
+        $boleto_cloud_token = '';
+        $location = '';
+
+        foreach($header_array as $h) {
+            if(preg_match('/X-BoletoCloud-Version:/i', $h)) {
+                $boleto_cloud_version = $h;
+            }
+            if(preg_match('/X-BoletoCloud-Token:/i', $h)) {
+                $boleto_cloud_token = $h;
+            }
+            if(preg_match('/Location:/i', $h)) {
+                $location = $h;
+            }
+        }
+
+        #Processando sucesso ou falha
+
+        if($http_code == $created){
+            #Versão da plataforma: $boleto_cloud_version 
+            #Token do boleto disponibilizado: $boleto_cloud_token
+            #Localização do boleto na plataforma: $location
+            #Enviando boleto como resposta:
+            header('Content-type: application/pdf');
+            header('Content-Disposition: inline; filename=arquivo-api-boleto-post-teste.pdf');
+            echo $body; #Visualização no navegador
+        }else{
+            #Versão da plataforma: $boleto_cloud_version 
+            #Códgio de erro HTTP: $http_code
+            #Enviando erro como resposta:
+            header('Content-Type: application/json; charset=utf-8');
+            echo $body; #Visualização no navegador
+        }
+    }
+
+}
