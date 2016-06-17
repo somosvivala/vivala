2dc89cfb3461fbbb1e64f2f90519bf2d44b00f34
brunoluizgr <brunoluizgr@gmail.com>
2016-06-17 09:29:25 -0300

    Tela Login/Cadastro minorfixes e tradução.

--- resources/views/mobilecadastro.blade.php
+++ resources/views/mobilecadastro.blade.php
@@ -8,36 +8,51 @@
         <a href="#" class="link-voltar">
             <i class="fa fa-chevron-left"></i>
         </a>
-        <div class="conteudo-mobile col-xs-10 col-xs-offset-1">
+        <div class="conteudo-mobile col-xs-10 col-xs-offset-1 margin-t-1 margin-b-1">
             @if (count($errors) > 0)
                 @foreach ($errors->all() as $error)
                 <span class="form-mobile-error">{{ $error }}</span>
                 @endforeach
             @endif
-            <span class="form-mobile-warning hide" id="senhas-nao-coincidem">As senhas não coincidem</span>
-              {!! Form::open(['url' => '/autenticacao/register']) !!}
-            <div class="row margin-t-3">
-            {!! Form::text("username", null, ['required'=>'required', 'class' => 'form-mobile', 'placeholder' => trans('global.lbl_name')]) !!}
-            </div>
-            <div class="row margin-t-1">
-            {!! Form::email("email", old('email'), ['required'=>'required','class' => 'form-mobile', 'placeholder' => trans('global.lbl_email')]) !!}
-            </div>
-            <div class="row margin-t-1">
-            {!! Form::password("password", ['required'=>'required','class' =>'form-mobile', 'placeholder' => trans('global.lbl_password')]) !!}
-            </div>
-            <div class="row margin-t-1">
-            {!! Form::password("password_confirmation", ['required'=>'required','class'=>'form-mobile', 'placeholder' => trans('global.lbl_password_confirm') ]) !!}
-            </div>
-            <div class="row margin-t-1">
-                {!! Form::submit( trans('global.lbl_register2') , ['class' => 'btn-mobile']) !!}
-            </div>
+            <span class="form-mobile-warning hide" id="senhas-nao-coincidem">
+              {!! trans('global.lbl_password_dont_match') !!}
+            </span>
+
+            {!! Form::open(['url' => '/autenticacao/register']) !!}
+              <div class="row margin-t-1">
+                <div class="col-xs-10 col-xs-offset-1">
+                  {!! Form::text("username", null, ['required'=>'required', 'class' => 'form-mobile', 'placeholder' => trans('global.lbl_name')]) !!}
+                </div>
+              </div>
+              <div class="row margin-t-1">
+                <div class="col-xs-10 col-xs-offset-1">
+                  {!! Form::email("email", old('email'), ['required'=>'required','class' => 'form-mobile', 'placeholder' => trans('global.lbl_email')]) !!}
+                </div>
+              </div>
+              <div class="row margin-t-1">
+                <div class="col-xs-10 col-xs-offset-1">
+                  {!! Form::password("password", ['required'=>'required','class' =>'form-mobile', 'placeholder' => trans('global.lbl_password')]) !!}
+                </div>
+              </div>
+              <div class="row margin-t-1">
+                <div class="col-xs-10 col-xs-offset-1">
+                  {!! Form::password("password_confirmation", ['required'=>'required','class'=>'form-mobile', 'placeholder' => trans('global.lbl_password_confirm') ]) !!}
+                </div>
+              </div>
+              <div class="row margin-t-2">
+                  {!! Form::submit( trans('global.lbl_register2') , ['class' => 'btn-mobile']) !!}
+              </div>
             {!! Form::close() !!}
-            <span class="texto-minusculo">{!! trans('global.lbl_or') !!}</span>
             <div class="row margin-t-1">
-                <a href="{{ url('/fbLogin') }}" class="btn-mobile btn-face" target="_self" rel="nofollow"> Conecte-se <span class="fa fa-facebook-square pull-right"></span> </a>
+              <span class="texto-minusculo">{!! trans('global.lbl_or') !!}</span>
             </div>
-            <div class="col-xs-12 margin-t-4 concorda-com">
+            <div class="row margin-t-1">
+                <a href="{{ url('/fbLogin') }}" class="btn-mobile btn-face" target="_self" rel="nofollow"> {!! trans('global.lbl_connect_yourself') !!} <span class="fa fa-facebook-square pull-right"></span> </a>
+            </div>
+            <div class="row margin-t-4">
+              <div class="col-xs-12 concorda-com">
                 <span>{!! trans('global.welcome_aboutprivacy4') !!} <a href="{{ url('/paginas/termosecondicoes') }}" class="link-verde">{!! trans('global.lbl_legal_terms') !!}</a> {!! trans('global.lbl_and') !!} <a href="{{ url('/paginas/termosecondicoes') }}" class="link-verde">{!! trans('global.lbl_data_policy') !!}</a>
+              </div>
             </div>
         </div>
     </div>
--- resources/views/mobilelogin.blade.php
+++ resources/views/mobilelogin.blade.php
@@ -9,33 +9,47 @@
             <i class="fa fa-chevron-left"></i>
         </a>
 
-        <div class="conteudo-mobile ">
+        <div class="conteudo-mobile col-xs-10 col-xs-offset-1 margin-t-1 margin-b-1">
            {!! Form::open(['url' => '/autenticacao/login', 'class' => 'form-horizontal form-login']) !!}
-            @if (count($errors) > 0)
-                @foreach ($errors->all() as $error)
-                <span class="form-mobile-error">{{ $error }}</span>
-                @endforeach
-            @endif
-            <div class="row margin-t-3">
-            {!! Form::email("email", old('email'), ['required'=>'required','class' => 'form-mobile', 'placeholder' => trans('global.lbl_email') ]) !!}
-            </div>
-            <div class="row margin-t-2">
-            {!! Form::password("password", ['required'=>'required', 'class' => 'form-mobile', 'placeholder' => trans('global.lbl_password') ]) !!}
-            </div>
-            <div class="row"><a href="{{ url('/password/email') }}"><small class="text-left width20em">{!! trans('global.lbl_password_forgot') !!}</small></a></div>
-            <div class="row margin-t-1 margin-t-1">
-                <input type="submit" class="btn-mobile btn-verde" value="Login">
-            </div>
+              @if (count($errors) > 0)
+                  @foreach ($errors->all() as $error)
+                  <span class="form-mobile-error">{{ $error }}</span>
+                  @endforeach
+              @endif
+              <div class="row margin-t-1">
+                <div class="col-xs-10 col-xs-offset-1">
+                  {!! Form::email("email", old('email'), ['required'=>'required','class' => 'form-mobile', 'placeholder' => trans('global.lbl_email') ]) !!}
+                </div>
+              </div>
+              <div class="row margin-t-2">
+                <div class="col-xs-10 col-xs-offset-1">
+                  {!! Form::password("password", ['required'=>'required', 'class' => 'form-mobile', 'placeholder' => trans('global.lbl_password') ]) !!}
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xs-10 col-xs-offset-1 text-left">
+                  <a href="{{ url('/password/email') }}">
+                    <small class="forgot-password">{!! trans('global.lbl_password_forgot') !!}</small>
+                  </a>
+                </div>
+              </div>
+              <div class="row margin-t-2">
+                  <input type="submit" class="btn-mobile btn-verde" value="Login">
+              </div>
             {!! Form::close() !!}
-            <div class="row margin-t-1">
+            <div class="row margin-t-2">
                 <a href="{{ url('/autenticacao/cadastro') }}" class="btn-mobile" target="_self" rel="nofollow"> Cadastre-se </a>
             </div>
-            <span class="texto-minusculo">{!! trans('global.lbl_or') !!}</span>
+            <div class="row margin-t-1">
+              <span class="texto-minusculo">{!! trans('global.lbl_or') !!}</span>
+            </div>
             <div class="row margin-t-1">
               <a href="{{ url('/fbLogin') }}" class="btn-mobile btn-face" target="_self" rel="nofollow"> Conecte-se <span class="fa fa-facebook-square pull-right"></span> </a>
             </div>
-            <div class="col-xs-12 margin-t-4 concorda-com">
-              <span>{!! trans('global.welcome_aboutprivacy4') !!} <a href="{{ url('/paginas/termosecondicoes') }}" class="link-verde">{!! trans('global.lbl_legal_terms') !!}</a> {!! trans('global.lbl_and') !!} <a href="{{ url('/paginas/termosecondicoes') }}" class="link-verde">{!! trans('global.lbl_data_policy') !!}</a>
+            <div class="row margin-t-4">
+              <div class="col-xs-12 concorda-com">
+                <span>{!! trans('global.welcome_aboutprivacy4') !!} <a href="{{ url('/paginas/termosecondicoes') }}" class="link-verde">{!! trans('global.lbl_legal_terms') !!}</a> {!! trans('global.lbl_and') !!} <a href="{{ url('/paginas/termosecondicoes') }}" class="link-verde">{!! trans('global.lbl_data_policy') !!}</a>
+              </div>
             </div>
         </div>
     </div>
