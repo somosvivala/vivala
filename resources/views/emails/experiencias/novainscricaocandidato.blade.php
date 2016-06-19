fbcd12b6a866d8abd0c120705715b3319030b4be
brunoluizgr <brunoluizgr@gmail.com>
2016-06-19 17:20:33 -0300

    Mail base criado. header/footer setados e miolo pronto para as informações, TODO: adicionar ao ENV links da Vivalá.

--- resources/views/emails/experiencias/novainscricaocandidato.blade.php
+++ resources/views/emails/experiencias/novainscricaocandidato.blade.php
@@ -120,7 +120,7 @@
 
       }
       table.footer-wrap a {
-        color: #999999;
+        color: transparent;
       }
 
       /* ---------------------------------------------------
@@ -149,68 +149,54 @@
       }
       </style>
   </head>
-  <body bgcolor="#f6f6f6">
+  <body bgcolor="#D1D3D4" style="padding-top:15px; padding-right:15px; padding-bottom:45px; padding-left:15px; ">
     <!-- body -->
-    <table class="body-wrap" bgcolor="#f6f6f6">
-      <tr>
-        <td></td>
-        <td class="container" bgcolor="#FFFFFF">
-
-          <!-- content -->
-          <div class="content">
-          <table>
-            <tr>
-              <td>
-                <p>Hi there,</p>
-                <p>Sometimes all you want is to send a simple HTML email with a basic design.</p>
-                <h1>Really simple HTML email template</h1>
-                <p>This is a really simple email template. Its sole purpose is to get you to click the button below.</p>
-                <h2>How do I use it?</h2>
-                <p>All the information you need is on GitHub.</p>
-                <!-- button -->
-                <table class="btn-primary" cellpadding="0" cellspacing="0" border="0">
-                  <tr>
-                    <td>
-                      <a href="https://github.com/leemunroe/html-email-template">View the source and instructions on GitHub</a>
-                    </td>
-                  </tr>
-                </table>
-                <!-- /button -->
-                <p>Feel free to use, copy, modify this email template as you wish.</p>
-                <p>Thanks, have a lovely day.</p>
-                <p><a href="http://twitter.com/leemunroe">Follow @leemunroe on Twitter</a></p>
-              </td>
-            </tr>
-          </table>
-          </div>
-          <!-- /content -->
-
-        </td>
-        <td></td>
-      </tr>
+    <table>
+      <table class="body-wrap" bgcolor="#F06F37" style="margin-bottom:10px;">
+        <tr>
+          <td align-"center">
+            <a href="https://vivala.com.br" target="_blank">
+              <img src="{{ asset('logo-branco.png') }}" alt="Vivalá" style="display:block; margin:auto;" border="0" width="100%" max-width="150px" height="100%" max-height="82px">
+            </a>
+          </td>
+        </tr>
+      </table>
+      <table class="body-wrap" bgcolor="#FFFFFF" style="margin-bottom:10px;">
+        <tr>
+          <td>
+          </td>
+        </tr>
+      </table>
     </table>
     <!-- /body -->
     <!-- footer -->
     <table class="footer-wrap">
       <tr>
-        <td></td>
         <td class="container">
-
-          <!-- content -->
           <div class="content">
             <table>
               <tr>
                 <td align="center">
-                  <p>Don't like these annoying emails? <a href="#"><unsubscribe>Unsubscribe</unsubscribe></a>.
-                  </p>
+                  <a href="{{ env('VIVALA_LINK_FACEBOOK') }}" target="_blank">
+                    <img src="{{ asset('img/email/vivala-email-fb-cor-icone.png') }}"/>
+                  </a>
+                  <a href="{{ env('VIVALA_LINK_INSTAGRAM') }}" target="_blank">
+                    <img src="{{ asset('img/email/vivala-email-ig-cor-icone.png') }}"/>
+                  </a>
+                  <a href="{{ env('VIVALA_LINK_SITE') }}" target="_blank">
+                    <img src="{{ asset('img/email/vivala-email-link-cor-icone.png') }}"/>
+                  </a>
+                  <a href="mailto:{{ env('VIVALA_LINK_EMAIL') }}" target="_top">
+                    <img src="{{ asset('img/email/vivala-email-mail-cor-icone.png') }}"/>
+                  </a>
+                  <a href="{{ env('VIVALA_LINK_LINKEDIN') }}" target="_blank">
+                    <img src="{{ asset('img/email/vivala-email-in-cor-icone.png') }}"/>
+                  </a>
                 </td>
               </tr>
             </table>
           </div>
-          <!-- /content -->
-
         </td>
-        <td></td>
       </tr>
     </table>
     <!-- /footer -->
