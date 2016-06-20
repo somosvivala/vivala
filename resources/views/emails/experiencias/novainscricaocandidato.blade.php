28a313f4858769f33a4d7f331ab7a64d345e887b
brunoluizgr <brunoluizgr@gmail.com>
2016-06-19 22:35:47 -0300

    Estrutura parcial do email de novaInscricaoCandidato.

--- resources/views/emails/experiencias/novainscricaocandidato.blade.php
+++ resources/views/emails/experiencias/novainscricaocandidato.blade.php
@@ -5,40 +5,89 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <style>
-      /* -------------------------------------
-          GLOBAL
-      ------------------------------------- */
-      * {
-        font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
-        font-size: 100%;
-        line-height: 1.6em;
-        margin: 0;
-        padding: 0;
+    /***************************************
+    * FONTES
+    ***************************************/
+      @font-face {
+        font-family: 'Avenir Light';
+        src: url('/fonts/avenir/Avenir-Light/Avenir-Light.eot');
+        src: url('/fonts/avenir/Avenir-Light/Avenir-Light?#iefix') format('embedded-opentype'),
+        url('/fonts/avenir/Avenir-Light/Avenir-Light.woff2') format('woff2'),
+        url('/fonts/avenir/Avenir-Light/Avenir-Light.woff') format('woff'),
+        url('/fonts/avenir/Avenir-Light/Avenir-Light.ttf') format('truetype'),
+        url('/fonts/avenir/Avenir-Light/Avenir-Light.svg#Avenir-Light') format('svg');
       }
-      img {
-        max-width: 600px;
-        width: auto;
+      @font-face{
+        font-family: 'Avenir Medium';
+        src: url('/fonts/avenir/Avenir-Medium/Avenir-Medium.eot');
+        src: url('/fonts/avenir/Avenir-Medium/Avenir-Medium?#iefix') format('embedded-opentype'),
+        url('/fonts/avenir/Avenir-Medium/Avenir-Medium.woff2') format('woff2'),
+        url('/fonts/avenir/Avenir-Medium/Avenir-Medium.woff') format('woff'),
+        url('/fonts/avenir/Avenir-Medium/Avenir-Medium.ttf') format('truetype'),
+        url('/fonts/avenir/Avenir-Medium/Avenir-Medium.svg#Avenir-Medium') format('svg');
       }
-      body {
-        -webkit-font-smoothing: antialiased;
-        height: 100%;
-        -webkit-text-size-adjust: none;
-        width: 100% !important;
+      @font-face{
+        font-family: 'Avenir Roman';
+        src: url('/fonts/avenir/Avenir-Roman/Avenir-Roman.eot');
+        src: url('/fonts/avenir/Avenir-Roman/Avenir-Roman?#iefix') format('embedded-opentype'),
+        url('/fonts/avenir/Avenir-Roman/Avenir-Roman.woff2') format('woff2'),
+        url('/fonts/avenir/Avenir-Roman/Avenir-Roman.woff') format('woff'),
+        url('/fonts/avenir/Avenir-Roman/Avenir-Roman.ttf') format('truetype'),
+        url('/fonts/avenir/Avenir-Roman/Avenir-Roman.svg#Avenir-Roman') format('svg');
       }
-      /* -------------------------------------
-          TIPOGRAFIA
-      ------------------------------------- */
+      @font-face{
+        font-family: 'Avenir Black';
+        src: url('/fonts/avenir/Avenir-Black/Avenir-Black.eot');
+        src: url('/fonts/avenir/Avenir-Black/Avenir-Black?#iefix') format('embedded-opentype'),
+        url('/fonts/avenir/Avenir-Black/Avenir-Black.woff2') format('woff2'),
+        url('/fonts/avenir/Avenir-Black/Avenir-Black.woff') format('woff'),
+        url('/fonts/avenir/Avenir-Black/Avenir-Black.ttf') format('truetype'),
+        url('/fonts/avenir/Avenir-Black/Avenir-Black.svg#Avenir-Black') format('svg');
+      }
+      @font-face{
+        font-family: 'FuturaBT Bold';
+        src: url('/fonts/futura/FuturaBT-Bold/Futura-BTBold.eot');
+        src: url('/fonts/futura/FuturaBT-Bold/Futura-BTBold?#iefix') format('embedded-opentype'),
+        url('/fonts/futura/FuturaBT-Bold/FuturaBT-Bold.woff2') format('woff2'),
+        url('/fonts/futura/FuturaBT-Bold/FuturaBT-Bold.woff') format('woff'),
+        url('/fonts/futura/FuturaBT-Bold/FuturaBT-Bold.ttf') format('truetype'),
+        url('/fonts/futura/FuturaBT-Bold/FuturaBT-Bold.svg#FuturaBT-Bold') format('svg');
+      }
+
+      /***************************************
+      * GLOBAL
+      ***************************************/
+        * {
+          font-family: "Avenir Roman", "Helvetica", Helvetica, Arial, sans-serif;
+          font-size: 100%;
+          line-height: 1.6em;
+          margin: 0;
+          padding: 0;
+        }
+        img {
+          max-width: 600px;
+          width: auto;
+        }
+        body {
+          -webkit-font-smoothing: antialiased;
+          height: 100%;
+          -webkit-text-size-adjust: none;
+          width: 100% !important;
+        }
+      /***************************************
+      * TIPOGRAFIA
+      ***************************************/
       h1,
       h2,
       h3 {
         color: #111111;
-        font-family: "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+        font-family: "FuturaBT Bold", Helvetica, Arial, "Lucida Grande", sans-serif;
         font-weight: 200;
         line-height: 1.2em;
         margin: 40px 0 10px;
       }
       h1 {
-        font-size: 36px;
+        font-size: 30px;
       }
       h2 {
         font-size: 28px;
@@ -58,9 +107,9 @@
         margin-left: 5px;
         list-style-position: inside;
       }
-      /* -------------------------------------
-          ELEMENTOS
-      ------------------------------------- */
+      /***************************************
+      * ELEMENTOS
+      ***************************************/
       a {
         color: #348eda;
       }
@@ -71,7 +120,7 @@
       .btn-primary td {
         background-color: #348eda;
         border-radius: 25px;
-        font-family: "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+        font-family: 'Avenir-Roman', Helvetica, Arial, "Lucida Grande", sans-serif;
         font-size: 14px;
         text-align: center;
         vertical-align: top;
@@ -97,9 +146,9 @@
       .padding {
         padding: 10px 0;
       }
-      /* -------------------------------------
-          CORPO
-      ------------------------------------- */
+      /***************************************
+      * CORPO
+      ***************************************/
       table.body-wrap {
         padding: 20px;
         width: 100%;
@@ -107,9 +156,9 @@
       table.body-wrap .container {
         border: 1px solid #f0f0f0;
       }
-      /* -------------------------------------
-          RODAPÉ
-      ------------------------------------- */
+      /***************************************
+      * RODAPÉ
+      ***************************************/
       table.footer-wrap {
         clear: both !important;
         width: 100%;
@@ -122,10 +171,9 @@
       table.footer-wrap a {
         color: transparent;
       }
-
-      /* ---------------------------------------------------
-          RESPONSIVIDADE
-      ------------------------------------------------------ */
+      /***************************************
+      * RESPONSIVIDADE
+      ***************************************/
       /* Set a max-width, and make it display as block so it will automatically stretch to that width, but will also shrink down on a phone or something */
       .container {
         clear: both !important;
@@ -150,26 +198,135 @@
       </style>
   </head>
   <body bgcolor="#D1D3D4" style="padding-top:15px; padding-right:15px; padding-bottom:45px; padding-left:15px; ">
-    <!-- body -->
-    <table>
+    <!-- CORPO DO EMAIL -->
+    <table class="body-wrap" style="padding: 0 20px;">
+      <!-- Cabeçalho da VIVALÁ -->
       <table class="body-wrap" bgcolor="#F06F37" style="margin-bottom:10px;">
-        <tr>
-          <td align-"center">
+        <!-- Logo da VIVALÁ -->
+        <tr align-"center">
+          <td>
             <a href="https://vivala.com.br" target="_blank">
               <img src="{{ asset('logo-branco.png') }}" alt="Vivalá" style="display:block; margin:auto;" border="0" width="100%" max-width="150px" height="100%" max-height="82px">
             </a>
           </td>
         </tr>
-      </table>
-      <table class="body-wrap" bgcolor="#FFFFFF" style="margin-bottom:10px;">
-        <tr>
+        <!-- Fim do Logo da VIVALÁ -->
+        <!-- Divisor -->
+        <tr align="center">
           <td>
+            <div style="border-bottom: 1px solid #FFFFFF;width: 300px;margin: 25px 0;"></div>
           </td>
         </tr>
+        <!-- Fim do Divisor -->
+        <!-- Título do EMAIL -->
+        <tr align="center">
+          <td>
+            <h3 style="color:#FFFFFF;margin-top:0;">Falta apenas um passo para sua experiência acontecer!</h3>
+          </td>
+        </tr>
+        <!-- Fim do Título do EMAIL -->
       </table>
+      <!-- Fim do Cabeçalho da VIVALÁ -->
+      <!-- Primeira Estrutura -->
+      <table class="body-wrap" bgcolor="#FFFFFF" style="padding-bottom:0;">
+        <!-- Título da Primeira Estrutura -->
+        <tr align="center">
+          <td>
+            <h1 style="color:#FAA325;">Você está quase lá!</h1>
+          </td>
+        </tr>
+        <!-- Fim do Título da Primeira Estrutura -->
+        <!-- Imagem da Primeira Estrutura -->
+        <tr align="center">
+          <td>
+            <img src="{{ asset('img/email/vivala-pagamento-pendente-icone-2.png') }}" width="99px;" height="99px;"/>
+          </td>
+        </tr>
+        <!-- Fim da Imagem da Primeira Estrutura -->
+        <!-- Seção de INFOS BANCÁRIAS -->
+        <tr align="center">
+          <td>
+            <p>Para confirmar sua inscrição na experiência da <strong>NOME INSTITUIÇÃO</strong></p>
+            <p>realize o depósito de <strong>R$VAL.OR</strong> na conta a seguir:</p>
+          </td>
+        </tr>
+        <tr align="center">
+          <td>
+            <div style="background-color:#ECEBEB; text-align:left; padding:10px; max-width:300px; margin-left:20px; margin-right:20px;">
+              <p style="color:#545454;"><b>CONTA</b> <span>420.420.420-42</span></p>
+              <p style="color:#545454;"><b>AGÊNCIA</b> <span>0420</span></p>
+              <p style="color:#545454;"><b>CPF/CNPJ</b> <span>420.420.420-42</span></p>
+              <p style="color:#545454;"><b>BANCO</b> <span>Banco dos BR</span></p>
+            </div>
+          </td>
+        </tr>
+        <!-- Fim da Seção de INFOS BANCÁRIAS -->
+        <!-- Seção do DEPÓSITO -->
+        <tr align="center">
+          <td>
+            <p style="margin-top:30px;">Não se esqueça de nos enviar o comprovante clicando no botão abaixo!</p>
+          </td>
+        </tr>
+        <tr align="center">
+          <td>
+            <button style="font-family: Avenir Black; font-size: 20px; text-transform: uppercase; color:#F06F37; background-color:#FFFFFF; padding:15px 30px; border:1px solid #F06F37; margin-left:20px; margin-right:20px;">ENVIAR COMPROVANTE DE DEPÓSITO</button>
+          </td>
+        </tr>
+        <!-- Fim da Seção do DEPÓSITO -->
+        <!-- Seção do BOLETO BANCÁRIO -->
+        <tr align="center">
+          <td>
+            <p style="margin-top:30px;">Ou pague com boleto bancário:</p>
+          </td>
+        </tr>
+        <tr align="center">
+          <td>
+            <div style="background-color:#FFFFFF; padding:15px 30px; border:1px solid #25A494; max-width:200px;">
+              <span>
+                <img style="vertical-align:middle;" src="{{ asset('img/email/vivala-email-codigo-de-barras-icone.png') }}"/>
+                <span style="font-family:Avenir Black; font-size:16px; color:#25A494; text-transform:uppercase; margin-left: 15px;">GERAR BOLETO</span>
+              </span>
+            </div>
+          </td>
+        </tr>
+        <!-- Fim da Seção do BOLETO BANCÁRIO -->
+        <!-- Divisor -->
+        <tr align="center">
+          <td>
+            <div style="border-bottom:1px solid #DCDEDF; width:500px; margin:50px 0;"></div>
+          </td>
+        </tr>
+        <!-- Fim do Divisor -->
+      </table>
+      <!-- Fim da Primeira Estrutura -->
+      <!-- Segunda Estrutura -->
+      <table class="body-wrap" bgcolor="#FFFFFF" style="padding-top:0; margin-bottom:10px;">
+        <!-- Infos da EXPERIÊNCIA -->
+        <tr>
+          <td style="padding-left:50px; margin-bottom:15px;">
+            <h4 style="color:#545454;">
+              Detalhes da experiência
+            </h4>
+          </td>
+        </tr>
+        <tr>
+          <td style="padding-left:65px;">
+            <span>
+              <img src="{{ asset('img/dummy-exp.jpg') }}" width="220px" height="220px"/>
+            </span>
+            <span>
+              <p></p>
+              <p></p>
+              <p>Cras euismod sit amet est ut elementum. Proin egestas, ante non aliquam fermentum, mauris ex fringilla quam, vel accumsan justo tellus nec odio. Pellentesque tempus magna dictum leo eleifend vulputate.</p>
+            </span>
+          </td>
+        </tr>
+        <!-- Fim das Infos da EXPERIÊNCIA -->
+      </table>
+      <!-- Fim da Segunda Estrutura -->
     </table>
-    <!-- /body -->
-    <!-- footer -->
+    <!-- Fim do CORPO DO EMAIL -->
+    <!-- RODAPÉ -->
     <table class="footer-wrap">
       <tr>
         <td class="container">
