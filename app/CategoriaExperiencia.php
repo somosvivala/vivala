dee230ff6db38c1db2502b0f4d335c84c71920e2
Renato Gomes <renatoo.agomes@gmail.com>
2016-08-15 17:16:45 -0300

    Commit com acessor para o path do icone em png de uma CategoriaExperiencia ou de uma InformacaoExperiencia ( usar o atributo ->pathIconePNG de uma instancia de CategoriaExperiencia ou InformacaoExperiencia)

--- app/CategoriaExperiencia.php
+++ app/CategoriaExperiencia.php
@@ -9,12 +9,42 @@ class CategoriaExperiencia extends Model {
       'icone'
     ];
 
+
+    /*
+     * ### Relações entre as entidades ###
+     */
+
     /**
      * Uma CategoriaExperiencia pertence a muitas Experiencias
      */
     public function experiencias()
     {
         return $this->belongsToMany('App\Experiencia');
+    }
+
+
+
+    /**
+     * ### Acessors (modificacoes que executarão após recuperar o campo do BD) ###
+     * ### (antes de devolver) ###
+     */
+
+
+    /**
+     * Definindo um acessor para o shortname do icone do font-awesome
+     */
+    public function getIconeShortNameAttribute()
+    {
+        return str_replace('fa ',  '', $this->icone);
+    }
+
+
+    /**
+     * Definindo um acessor para o path publico do icone dessa categoria
+     */
+    public function getPathIconePNGAttribute()
+    {
+        return url('/public/fapng/'.$this->iconeShortName.'.png');
     }
 
 
--- app/InformacaoExperiencia.php
+++ app/InformacaoExperiencia.php
@@ -18,4 +18,26 @@ class InformacaoExperiencia extends Model {
         return $this->belongsTo('App\Experiencia');
     }
 
+    /**
+     * ### Acessors (modificacoes que executarão após recuperar o campo do BD) ###
+     * ### (antes de devolver) ###
+     */
+
+    /**
+     * Definindo um acessor para o shortname do icone do font-awesome
+     */
+    public function getIconeShortNameAttribute()
+    {
+        return str_replace('fa ',  '', $this->icone);
+    }
+
+
+    /**
+     * Definindo um acessor para o path publico do icone dessa categoria
+     */
+    public function getPathIconePNGAttribute()
+    {
+        return url('/public/fapng/'.$this->iconeShortName.'.png');
+    }
+
 }
