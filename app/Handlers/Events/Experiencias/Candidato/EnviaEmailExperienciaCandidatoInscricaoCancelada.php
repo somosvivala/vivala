dea9bd265fe66cd25e4a3af8c0c4c7fcbfff1150
brunoluizgr <brunoluizgr@gmail.com>
2016-08-20 16:05:45 -0300

    Experiencias: Flow dos emails - Inscrever/Confirmar/Cancelar -> Candidato.

--- app/Handlers/Events/Experiencias/Candidato/EnviaEmailExperienciaCandidatoInscricaoCancelada.php
+++ app/Handlers/Events/Experiencias/Candidato/EnviaEmailExperienciaCandidatoInscricaoCancelada.php
@@ -5,27 +5,35 @@ use App\Events\Experiencias\InscricaoExperienciaCancelada;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldBeQueued;
 
-class EnviaEmailExperienciaCandidatoInscricaoCancelada {
+use App\InscricaoExperiencia;
+use App\Repositories\MailSenderRepository;
+
+class EnviaEmailExperienciaCandidatoInscricaoCancelada
+{
+	// Cria instância de mailSenderRepository para ser usada aqui
+	private $MailSenderRepository;
 
 	/**
-	 * Create the event handler.
-	 *
-	 * @return void
-	 */
+	* Create the event handler.
+	*
+	* @return void
+	*/
 	public function __construct()
 	{
-		//
+		// A instância criada aqui (this) recebe o repositório tipo MailSenderRepository
+		$this->MailSenderRepository = $repository;
 	}
 
 	/**
-	 * Handle the event.
-	 *
-	 * @param  InscricaoExperienciaCancelada  $event
-	 * @return void
-	 */
+	* Handle the event.
+	*
+	* @param  InscricaoExperienciaCancelada  $event
+	* @return void
+	*/
 	public function handle(InscricaoExperienciaCancelada $event)
 	{
-		//
+		// Usa o método enviaEmailExperienciaCandidatoInscricaoCancelada do mailSenderRepository para enviar o email
+		$this->MailSenderRepository->enviaEmailExperienciaCandidatoInscricaoCancelada($event->Inscricao);
 	}
 
 }
