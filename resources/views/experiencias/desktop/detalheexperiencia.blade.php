891ecb8748a32d217ba72d5ad63d3cb9a365e276
brunoluizgr <brunoluizgr@gmail.com>
2016-08-20 23:34:53 -0300

    Experiências: Refeitas as views do desktop da LISTAGEM de Experiências, INTERNA de Experiências (parcial: falta adicionar as redes sociais da instituição e também alterar/modificar os botões de Inscrição/Cancelamento. TODO: Voltar os handlers de emails (eminente,pendente,etc) antigos para a plataforma, revisar a view de checkout e modificar a estrutura.

--- resources/views/experiencias/desktop/detalheexperiencia.blade.php
+++ resources/views/experiencias/desktop/detalheexperiencia.blade.php
@@ -45,141 +45,159 @@
       </ul>
       <div class="tab-content">
         <div id="experiencia" class="tab-pane active" role="tabpanel">
-          <div class="row row-eq-height">
-            <div class="col-lg-5">
-              <div class="foto-img-interna-desktop" style="background-image:url('{{ $Experiencia->fotoCapaUrl}}')">
-                <div class="categorias-experiencia">
-                  <?php $contadorCategorias = 0; ?>
-                  @foreach($Experiencia->categorias as $Categoria)
-                    <div class="icone">
-                      <i class="fa-1-5x {{ $Categoria->icone }}" title="{{ $Categoria->nome }}" alt="{{ $Categoria->nome }}"></i>
-                    </div>
-                    {{-- A img do desktop/listagem só comporta 4 ícones por vez --}}
-                    @if(++$contadorCategorias === 7)
-                      <?php break; ?>
-                    @endif
-                  @endforeach
+
+          {{-- Seção de DESCRIÇÃO DA EXPERIÊNCIA --}}
+            <div class="row row-eq-height margin-b-2">
+              <div class="col-lg-7">
+                <div class="row">
+                  <div class="col-lg-12 text-center">
+                    <p class="margin-b-0-5 ajuste-fonte-avenir-black laranja">Sobre a Experiência</p>
+                  </div>
+                  <div class="col-lg-12">
+                    <p class="ajuste-fonte-avenir-light">{{ $Experiencia->descricao }}</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-5">
+                <div class="foto-img-interna-desktop" style="background-image:url('{{ $Experiencia->fotoCapaUrl}}')">
+                  <div class="categorias-experiencia">
+                    <?php $contadorCategorias = 0; ?>
+                    @foreach($Experiencia->categorias as $Categoria)
+                      <div class="icone">
+                        <i class="fa-1-5x {{ $Categoria->icone }}" title="{{ $Categoria->nome }}" alt="{{ $Categoria->nome }}"></i>
+                      </div>
+                      {{-- A img do desktop/listagem só comporta 4 ícones por vez --}}
+                      @if(++$contadorCategorias === 7)
+                        <?php break; ?>
+                      @endif
+                    @endforeach
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="col-lg-7 descricao">
-              <div class="row">
-                <span class="col-lg-12"><i class="fa fa-fw fa-clock-o"></i> {{ $Experiencia->frequencia }}</span>
-                <span class="col-lg-12"><i class="fa fa-fw fa-map-marker"></i> {{ $Experiencia->local->nome}} - {{ $Experiencia->local->estado->sigla }}</span>
-                <span class="col-lg-12"><i class="fa fa-fw fa-money"></i> R${{ $Experiencia->preco }}</span>
-                <span class="col-lg-12">{{ $Experiencia->descricao }}</span>
-                    {!! Form::hidden('experiencia_tipo', $Experiencia->tipo) !!}
-                    {!! Form::hidden('experiencia_id', $Experiencia->id) !!}
-                    {!! Form::hidden('_token', csrf_token()) !!}
-              </div>
-            </div>
-          </div>
+          {{-- Fim da Seção de DESCRIÇÃO DA EXPERIÊNCIA --}}
 
-          {{-- Seção DATAS DA EXPERIÊNCIA --}}
-            <div class="row margin-b-2">
+          {{-- Seção de INFORMAÇÕES BÁSICAS --}}
+            <div class="row margin-b-3">
               <div class="col-lg-12">
-                <span class="col-lg-12 negrito-exp margin-t-1">Data</span>
-                @if($Experiencia->isEventoUnico)
-                  <div class="col-lg-12 informacoes">
-                    <div class="row padding-t-1">
-                      <span class="icone-informacoes"><i class="fa fa-calendar"></i></span>
-                      <span class="descricao-informacoes">{{ $Experiencia->dataProximaOcorrencia }}</span>
+                <div class="col-lg-6">
+                  <p class="margin-b-0 laranja">
+                    <i class="fa fa-fw fa-clock-o"></i>
+                    <span class="ajuste-fonte-avenir-black">Quando?</span>
+                  </p>
+                  <p class="ajuste-fonte-avenir-light">{{ $Experiencia->frequencia }}</p>
+                </div>
+                <div class="col-lg-6">
+                  <p class="margin-b-0 laranja">
+                    <i class="fa fa-fw fa-money"></i>
+                    <span class="ajuste-fonte-avenir-black">Quanto?</span>
+                  </p>
+                  <p class="ajuste-fonte-avenir-light">R${{ $Experiencia->preco }}</p>
+                </div>
+                <div class="col-lg-6">
+                  <p class="margin-b-0 laranja">
+                    <i class="fa fa-fw fa-map-marker"></i>
+                    <span class="ajuste-fonte-avenir-black">Onde?</span>
+                  </p>
+                  <p class="ajuste-fonte-avenir-light">{{ $Experiencia->local->nome}} - {{ $Experiencia->local->estado->sigla }}</p>
+                </div>
+                <div class="col-lg-6">
+                  <p class="margin-b-0 laranja">
+                    <i class="fa fa-fw fa-calendar-o"></i>
+                    <span class="ajuste-fonte-avenir-black">Datas Disponíveis</span>
+                  </p>
+                  @if($Experiencia->isEventoUnico)
+                    <div class="col-lg-12 informacoes">
+                      <div class="row">
+                        <span class="descricao-informacoes">{{ $Experiencia->dataProximaOcorrencia }}</span>
+                      </div>
                     </div>
-                  </div>
-                @endif
-                @if($Experiencia->isEventoRecorrente)
-                  <div class="col-lg-12 informacoes">
-                    <div class="row padding-t-1">
-                      <span class="icone-informacoes"><i class="fa fa-calendar"></i></span>
-                      <span class="descricao-informacoes">
+                  @endif
+                  @if($Experiencia->isEventoRecorrente)
+                    <div class="col-lg-12 informacoes">
+                      <div class="row">
+                        <span class="ajuste-fonte-avenir-light">Selecione uma data disponível</span>
+                        <span class="descricao-informacoes">
+                            <input type="date" class="clndr-picker" placeholder="Escolha uma data" name="data-escolhida" readonly>
+                            <div class="clndr-container">
+                            </div>
+                            <input type="hidden" id="json-eventos" value='{{ $Experiencia->proximasOcorrenciasJSON }}'>
+                        </span>
+                      </div>
+                    </div>
+                  @endif
+                  @if($Experiencia->isEventoServico)
+                    <div class="col-lg-12 informacoes">
+                      <div class="row">
+                        <span class="ajuste-fonte-avenir-light">Selecione uma data disponível</span>
+                        <span class="descricao-informacoes">
                           <input type="date" class="clndr-picker" placeholder="Escolha uma data" name="data-escolhida" readonly>
                           <div class="clndr-container">
                           </div>
-                          <input type="hidden" id="json-eventos" value='{{ $Experiencia->proximasOcorrenciasJSON }}'>
-                      </span>
+                          <input type="hidden" id="json-eventos" value='{{ $Experiencia->diasOperacionaisJSON }}'>
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                @endif
-                @if($Experiencia->isEventoServico)
-                  <div class="col-lg-12 informacoes">
-                    <div class="row padding-t-1">
-                      <span class="icone-informacoes"><i class="fa fa-calendar"></i></span>
-                      <span class="descricao-informacoes">
-                        <input type="date" class="clndr-picker" placeholder="Escolha uma data" name="data-escolhida" readonly>
-                        <div class="clndr-container">
-                        </div>
-                        <input type="hidden" id="json-eventos" value='{{ $Experiencia->diasOperacionaisJSON }}'>
-                      </span>
-                    </div>
-                  </div>
-                @endif
-              </div>
-            </div>
-          {{-- Fim da Seção DATAS DA EXPERIÊNCIA --}}
-
-          {{-- Seção INFORMAÇÕES DA EXPERIÊNCIA --}}
-            <div class="row margin-b-2">
-              <div class="col-lg-12">
-                <span class="col-lg-12 margin-b-1 negrito-exp">Informações</span>
-                <div class="col-lg-12">
-                  @foreach($Experiencia->informacoes as $Informacao)
-                  <div class="'col-lg-<?php if(count($Experiencia->informacoes) <= 4) $varColuna = '12'; else $varColuna = '6'; echo($varColuna).' informacoes'?>">
-                    <div class="col-lg-1"><i class="fa-1-5x {{ $Informacao->icone }}"></i></div>
-                    <div class="col-lg-11">{{ $Informacao->descricao }}</div>
-                  </div>
-                  @endforeach
+                  @endif
                 </div>
+                {!! Form::hidden('experiencia_tipo', $Experiencia->tipo) !!}
+                {!! Form::hidden('experiencia_id', $Experiencia->id) !!}
+                {!! Form::hidden('_token', csrf_token()) !!}
               </div>
             </div>
-          {{-- Fim da Seção INFORMAÇÕES DA EXPERIÊNCIA --}}
+          {{-- Fim da Seção de INFORMAÇÕES BÁSICAS --}}
 
-          {{-- Seção DETALHES DA EXPERIÊNCIA [Opcional] --}}
+          {{-- Seção de DETALHES DA EXPERIÊNCIA [Opcional] --}}
             @if($Experiencia->detalhes)
-              <div class="row margin-b-2">
+              <div class="row margin-b-3">
                 <div class="col-lg-12">
-                  <span class="col-lg-12 margin-t-1 margin-b-1 negrito-exp">Detalhes da experiência</span>
-                  <span class="col-lg-12">{{ $Experiencia->detalhes }}</span>
+                  <span class="col-lg-12 text-center margin-b-0-5 ajuste-fonte-avenir-black laranja">Detalhes da experiência</span>
+                  <span class="col-lg-12 text-justify ajuste-fonte-avenir-light">{{ $Experiencia->detalhes }}</span>
                 </div>
               </div>
             @endif
-          {{-- Fim da Seção DETALHES DA EXPERIÊNCIA [Opcional] --}}
+          {{-- Fim da Seção de DETALHES DA EXPERIÊNCIA [Opcional] --}}
 
-          {{-- Seção DETALHES DA INSTITUIÇÃO/ONG/EMPRESA --}}
+          {{-- Seção de INFORMAÇÕES EXTRAS DA EXPERIÊNCIA [Opcional] --}}
+            @if(!empty($Experiencia->informacoes))
+              <div class="row margin-b-3">
+                <div class="col-lg-12">
+                  <span class="col-lg-12 margin-b-1 ajuste-fonte-avenir-black laranja">Informações Extras</span>
+                  <div class="col-lg-12">
+                    @foreach($Experiencia->informacoes as $Informacao)
+                    <div class="'col-lg-<?php if(count($Experiencia->informacoes) <= 4) $varColuna = '12'; else $varColuna = '6'; echo($varColuna).' informacoes'?>">
+                      <div class="col-lg-1"><i class="fa-1-5x {{ $Informacao->icone }}"></i></div>
+                      <div class="col-lg-11 ajuste-fonte-avenir-light">{{ $Informacao->descricao }}</div>
+                    </div>
+                    @endforeach
+                  </div>
+                </div>
+              </div>
+            @endif
+          {{-- Fim da Seção de INFORMAÇÕES EXTRAS DA EXPERIÊNCIA [Opcional] --}}
+
+          {{-- Seção de DETALHES DA INSTITUIÇÃO/ONG/EMPRESA --}}
             <div class="row">
+              <div class="row text-center">
+                <span class="col-lg-12 margin-b-1 ajuste-fonte-avenir-black laranja">Quem coordena?</span>
+              </div>
               <div class="col-lg-12">
-                <div class="col-lg-6 col-lg-offset-3">
+                <div class="col-lg-8 col-lg-offset-2">
                     <div class="row row-eq-height">
-                      <div class="col-lg-6">
+                      <div class="col-lg-8">
+                        <p class="margin-t-0-5 ajuste-fonte-avenir-light">{{ $Experiencia->owner_descricao }}</p>
+                      </div>
+                      <div class="col-lg-4 text-center">
                         <a href="{{ url('/experiencias/'.$Experiencia->id) }}" target="_blank">
                           <img src="{{ $Experiencia->getFotoOwnerUrlAttribute() }}" alt="{{ ucfirst($Experiencia->owner_nome) }}" title="{{ ucfirst($Experiencia->owner_nome) }}" class="img-responsive img-circle"/>
-                          <p>{{ $Experiencia->owner_nome }}</p>
+                          <p class="margin-t-0-5 ajuste-fonte-avenir-black laranja">{{ $Experiencia->owner_nome }}</p>
                         </a>
-                        <p>
-                          @if($Experiencia->owner->url_facebook)
-                          <span><a href="https://facebook.com/{{ $Experiencia->owner->url_facebook }}" target="_blank" style="color:transparent!important;">
-                            <img src="{{ asset('img/icones/png/cinza-mini-fb-circulo.png') }}" alt="{{ trans('global.social_network_facebook') }}" title="{{ trans('global.social_network_facebook') }}" min-width="17px" width="17px" max-width="17px" min-height="18px" height="18px" max-height="18px"/>
-                          </a></span>
-                          @endif
-                          @if($Experiencia->owner->url_instagram)
-                          <span><a href="https://instagram.com/{{ $Experiencia->owner->url_instagram }}" target="_blank" style="color:transparent!important;">
-                            <img src="{{ asset('img/icones/png/cinza-mini-ig-circulo.png') }}" alt="{{ trans('global.social_network_instagram') }}" title="{{ trans('global.social_network_instagram') }}" min-width="17px" width="17px" max-width="17px" min-height="18px" height="18px" max-height="18px"/>
-                          </a></span>
-                          @endif
-                          @if($Experiencia->owner->url_site)
-                          <span><a href="http://{{ $Experiencia->owner->url_site = preg_replace('#^www\.(.+\.)#i', '$1', $Experiencia->owner->url_site) }}" target="_blank" min-width="17px" style="color:transparent!important;">
-                            <img src="{{ asset('img/icones/png/cinza-mini-link-circulo.png') }}" alt="{{ trans('global.lbl_website') }}" title="{{ trans('global.lbl_website') }}" width="17px" max-width="17px" min-height="18px" height="18px" max-height="18px"/>
-                          </a></span>
-                          @endif
-                        </p>
-                      </div>
-                      <div class="col-lg-6">
-                        <p>{{ $Experiencia->owner_descricao }}</p>
                       </div>
                     </div>
                   </div>
               </div>
             </div>
-          {{-- Fim da Seção DETALHES DA INSTITUIÇÃO/ONG/EMPRESA --}}
+          {{-- Fim da Seção de DETALHES DA INSTITUIÇÃO/ONG/EMPRESA --}}
 
           <div class="row">
             <div class="row text-center padding-t-1 padding-b-2">
