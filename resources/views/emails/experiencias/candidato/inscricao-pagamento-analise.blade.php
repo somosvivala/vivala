4b7103915abd91dff6262302035a660ce06e97da
brunoluizgr <brunoluizgr@gmail.com>
2016-09-19 12:41:38 -0300

    [Experiências] Teste de emails, Inscrição Pendente - Candidato.

--- resources/views/emails/experiencias/candidato/inscricao-pagamento-analise.blade.php
+++ resources/views/emails/experiencias/candidato/inscricao-pagamento-analise.blade.php
@@ -1,36 +1,22 @@
-@include('emails._header', [
+@extends('emails.experiencias._base-experiencias', [
   'emailCabecalho' => 'Pagamento em análise',
   'emailTitulo' => 'Falta pouco para sua experiência acontecer!'
-]);
+])
 
-<!-- Primeira SEÇÃO -->
-  @include('emails.experiencias._experiencias_img-cabecalho', [
-    'corTextos' => '#FAA325',
-    'textoCima' => '',
-    'textoBaixo' => 'O seu pagamento foi recebido e está em análise. Logo entraremos em contato para confirmar a sua inscrição!',
-    'linkImagem' => 'img/icones/png/laranja-retorno-vazio.png',
-    'titleImagem' => 'Pagamento em Análise',
-    'altImagem' => 'Pagamento em Análise'
-  ]);
-<!-- Fim da Primeira SEÇÃO -->
+  @section('email-experiencia-cabecalho')
+    @include('emails.experiencias._experiencias_img-cabecalho', [
+      'expCorTextos' => '#FAA325',
+      'expTextoCima' => '',
+      'expTextoBaixo' => 'O seu pagamento foi recebido e está em análise. Logo entraremos em contato para confirmar a sua inscrição!',
+      'expLinkImagem' => 'img/icones/png/laranja-retorno-vazio.png',
+      'expAltImagem' => 'Pagamento em Análise',
+      'expTitleImagem' => 'Pagamento em Análise'
+    ])
+  @stop
 
-<!-- Separador -->
-  <td bgcolor="#ECEBEB" style="clear: both!important; display: block!important; margin:0 auto!important; max-width:600px!important; padding:7px 20px;">
-    <tr></tr>
-  </td>
-<!-- Fim do Separador -->
-
-<!-- Segunda SEÇÃO -->
-  @include('emails.experiencias._experiencias_info-candidato', [
-    'Inscricao' => $Inscricao,
-    'divisor' => false
-  ]);
-<!-- Fim da Segunda SEÇÃO -->
-
-<!-- Separador -->
-<td bgcolor="#ECEBEB" style="clear: both!important; display: block!important; margin:0 auto!important; max-width:600px!important; padding:5px 20px;">
-  <tr></tr>
-</td>
-<!-- Fim do Separador -->
-
-@include('emails._footer')
+  @section('email-experiencia-conteudo')
+    @include('emails.experiencias._experiencias_info-candidato', [
+      'Inscricao' => $Inscricao,
+      'divisor' => false
+    ])
+  @stop
--- resources/views/emails/experiencias/candidato/inscricao-pagamento-confirmado-experiencia-eminente.blade.php
+++ resources/views/emails/experiencias/candidato/inscricao-pagamento-confirmado-experiencia-eminente.blade.php
@@ -1,106 +1,18 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>{{ trans('global.title_vivala') }} - A sua experiência confirmada está chegando!</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <style>
-      @import url('https://vivala.com.br/fonts/avenir/avenir.css');
-      @import url('https://vivala.com.br/fonts/futura/futura.css');
-      @font-face {
-        font-family: 'Avenir Light';
-        src: url('https:///vivala.com.br/fonts/avenir/Avenir-Light/Avenir-Light.eot');
-        src: url('https:///vivala.com.br/fonts/avenir/Avenir-Light/Avenir-Light?#iefix') format('embedded-opentype'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Light/Avenir-Light.woff2') format('woff2'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Light/Avenir-Light.woff') format('woff'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Light/Avenir-Light.ttf') format('truetype'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Light/Avenir-Light.svg#Avenir-Light') format('svg');
-      }
-      @font-face{
-        font-family: 'Avenir Medium';
-        src: url('https:///vivala.com.br/fonts/avenir/Avenir-Medium/Avenir-Medium.eot');
-        src: url('https:///vivala.com.br/fonts/avenir/Avenir-Medium/Avenir-Medium?#iefix') format('embedded-opentype'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Medium/Avenir-Medium.woff2') format('woff2'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Medium/Avenir-Medium.woff') format('woff'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Medium/Avenir-Medium.ttf') format('truetype'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Medium/Avenir-Medium.svg#Avenir-Medium') format('svg');
-      }
-      @font-face{
-        font-family: 'Avenir Roman';
-        src: url('https:///vivala.com.br/fonts/avenir/Avenir-Roman/Avenir-Roman.eot');
-        src: url('https:///vivala.com.br/fonts/avenir/Avenir-Roman/Avenir-Roman?#iefix') format('embedded-opentype'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Roman/Avenir-Roman.woff2') format('woff2'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Roman/Avenir-Roman.woff') format('woff'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Roman/Avenir-Roman.ttf') format('truetype'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Roman/Avenir-Roman.svg#Avenir-Roman') format('svg');
-      }
-      @font-face{
-        font-family: 'Avenir Black';
-        src: url('https:///vivala.com.br/fonts/avenir/Avenir-Black/Avenir-Black.eot');
-        src: url('https:///vivala.com.br/fonts/avenir/Avenir-Black/Avenir-Black?#iefix') format('embedded-opentype'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Black/Avenir-Black.woff2') format('woff2'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Black/Avenir-Black.woff') format('woff'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Black/Avenir-Black.ttf') format('truetype'),
-        url('https:///vivala.com.br/fonts/avenir/Avenir-Black/Avenir-Black.svg#Avenir-Black') format('svg');
-      }
-      @font-face{
-        font-family: 'FuturaBT Bold';
-        src: url('https:///vivala.com.br/fonts/futura/FuturaBT-Bold/Futura-BTBold.eot');
-        src: url('https:///vivala.com.br/fonts/futura/FuturaBT-Bold/Futura-BTBold?#iefix') format('embedded-opentype'),
-        url('https:///vivala.com.br/fonts/futura/FuturaBT-Bold/FuturaBT-Bold.woff2') format('woff2'),
-        url('https:///vivala.com.br/fonts/futura/FuturaBT-Bold/FuturaBT-Bold.woff') format('woff'),
-        url('https:///vivala.com.br/fonts/futura/FuturaBT-Bold/FuturaBT-Bold.ttf') format('truetype'),
-        url('https:///vivala.com.br/fonts/futura/FuturaBT-Bold/FuturaBT-Bold.svg#FuturaBT-Bold') format('svg');
-      }
-    </style>
-    <link href='https://vivala.com.br/fonts/avenir/avenir.css' rel='stylesheet' type='text/css'>
-    <link href='https://vivala.com.br/fonts/futura/futura.css' rel='stylesheet' type='text/css'>
-  </head>
-  <body bgcolor="#ECEBEB" style="font-size: 100%; line-height: 1.6em; margin:0; padding:0; -webkit-font-smoothing:antialiased; height: 100%; -webkit-text-size-adjust:none; width: 100%!important;">
-    <!-- Corpo -->
-    <table bgcolor="#ECEBEB" style="padding:20px 20px 0 20px; width:100%;">
-      <tbody>
-        <tr>
-          <!-- Cabeçalho da Vivalá -->
-          <td bgcolor="#F06F37" style="clear:both!important; display:block!important; margin:0 auto!important; max-width:600px!important; padding:20px;">
-            <div style="display:block; margin:0 auto; max-width:600px;">
-              <table style="width: 100%;">
-                <tbody>
-                <!-- Logo da VIVALÁ -->
-                <tr align="center">
-                  <td>
-                    <a href="{{ env('VIVALA_LINK_SITE') }}" target="_blank">
-                      <img src="{{ asset('logo-branco.png') }}" alt="{{ trans('global.alt_vivala') }}" title="{{ trans('global.title_vivala') }}" border="0" min-width="107px" width="auto" max-width="600px" min-height="59px" height="59px" max-height="59px" style="display:block; margin:auto;">
-                    </a>
-                  </td>
-                </tr>
-                <!-- Fim do Logo da VIVALÁ -->
-                <!-- Separador -->
-                <tr align="center">
-                  <td>
-                    <div style="border-bottom:1px solid #FFFFFF; width:300px; margin:25px 0;"></div>
-                  </td>
-                </tr>
-                <!-- Fim do Separador -->
-                <!-- Título do EMAIL -->
-                <tr align="center">
-                  <td>
-                    <h2 style="font-family:'FuturaBT Bold', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:22px; font-weight:normal; color:#FFFFFF; margin:40px 0 10px; margin-top:0; line-height:40px;">
-                      A experiência que você se inscreveu está quase chegando!
-                    </h2>
-                  </td>
-                </tr>
-                <!-- Fim do Título do EMAIL -->
-                </tbody>
-              </table>
-            </div>
-          </td>
-          <!-- Fim do Cabeçalho da Vivalá -->
-          <!-- Separador -->
-          <td bgcolor="#ECEBEB" style="clear: both!important; display: block!important; margin:0 auto!important; max-width:600px!important; padding:7px 20px;">
-            <tr></tr>
-          </td>
-          <!-- Fim do Separador -->
+@extends('emails.experiencias._base-experiencias', [
+  'emailCabecalho' => 'A sua experiência confirmada está chegando!',
+  'emailTitulo' => 'A experiência que você se inscreveu está quase chegando!'
+])
+
+  @section('email-experiencia-cabecalho')
+    @include('emails.experiencias._experiencias_img-cabecalho', [
+      'expCorTextos' => '#FAA325',
+      'expTextoCima' => '',
+      'expTextoBaixo' => 'Confira os detalhes da Experiência e algumas dicas da Vivalá para que tudo corra bem!',
+      'expLinkImagem' => 'img/icones/png/laranja-info-vazio.png',
+      'expAltImagem' => 'Pagamento Confirmado',
+      'expTitleImagem' => 'Pagamento Confirmado'
+    ])
+  @stop
           <!-- Corpo do Email 1 -->
           <td bgcolor="#FFFFFF" style="clear:both!important; display:block!important; margin:0 auto!important; max-width:600px!important; padding:20px;">
             <div style="display:block; margin:0 auto; max-width:600px;">
@@ -110,15 +22,15 @@
                   <!-- Imagem da Primeira Estrutura -->
                   <tr align="center">
                     <td>
-                      <img src="{{ asset('img/icones/png/laranja-info-vazio.png') }}" alt="{{ trans('global.lbl_info_') }}" title="{{ trans('global.lbl_info_') }}" min-width="99px" width="auto" max-width="600px" min-height="150px" height="150px" max-height="150px" style="margin-bottom:0px;"/>
+                      <img src="{{ asset('') }}" alt="{{ trans('global.lbl_info_') }}" title="{{ trans('global.lbl_info_') }}" min-width="99px" width="auto" max-width="600px" min-height="150px" height="150px" max-height="150px" style="margin-bottom:0px;"/>
                     </td>
                   </tr>
                   <!-- Fim da Imagem da Primeira Estrutura -->
                   <!-- Título da Primeira Estrutura -->
                   <tr align="center">
                     <td>
-                      <h1 style="font-family:'FuturaBT Bold', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:20px; font-weight:bold; color:#FAA325; margin-bottom:20px; line-height:40px;">
-                        Confira os detalhes da Experiência e algumas dicas da Vivalá para que tudo corra bem!
+                      <h1 style="font-family:'FuturaBT Bold', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:20px; font-weight:bold; color:; margin-bottom:20px; line-height:40px;">
+
                       </h1>
                     </td>
                   </tr>
