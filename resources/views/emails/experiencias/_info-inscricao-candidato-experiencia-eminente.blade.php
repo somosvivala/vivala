d901700f2794120cf7abd93babaf87b4fde762ce
brunoluizgr <brunoluizgr@gmail.com>
2016-09-22 18:34:51 -0300

    [Experiências] Fix de Inscricao Candidato - Confirmada/ExperienciaHoje & ExperienciaEminente.

---
+++ resources/views/emails/experiencias/_info-inscricao-candidato-experiencia-eminente.blade.php
@@ -0,0 +1,29 @@
+<!-- Seção DICAS PARA A INSTITUIÇÃO SOBRE A EXPERIÊNCIA  -->
+<td bgcolor="#3EA790" style="clear:both!important; display:block!important; margin:0 auto!important; max-width:600px!important; padding:5px 20px;">
+  <div style="display:block; margin:0 auto; max-width:600px;">
+    <table style="width: 100%; margin-top:0; padding-bottom:0;">
+      <tbody>
+        <tr align="left">
+          <td>
+            <h3 style="font-family:'FuturaBT Bold', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; color:#FFFFFF; line-height:1.2em; margin: 5px 0 0 0;">Dicas</h3>
+            <ul>
+              <li style="font-family:'Avenir Roman', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:14px; color:white; margin:0 auto; padding-bottom:15px;">
+                Tenha certeza de que todos os equipamentos necessários para a realização da experiência estejam disponíveis.
+              </li>
+              <li style="font-family:'Avenir Roman', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:14px; color:white; margin:0 auto; padding-bottom:15px;">
+                Disponibilize telefones para contato caso haja alguma emergência.
+              </li>
+              <li style="font-family:'Avenir Roman', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:14px; color:white; margin:0 auto; padding-bottom:15px;">
+                A sua Experiência é a céu aberto? Garanta protetores solares e capas de chuva para seus convidados!
+              </li>
+              <li style="font-family:'Avenir Roman', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:14px; color:white; margin:0 auto">
+                Não se esqueça: Gentileza gera gentileza :)
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</td>
+<!-- Fim da Seção DICAS PARA A INSTITUIÇÃO SOBRE A EXPERIÊNCIA  -->
