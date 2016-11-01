38222fadff4c73b9c427a07871a1fe37e7a8241f
Bruno Luiz <brunoluizgr@gmail.com>
2016-11-01 20:18:52 -0200

    [Experiências] Fix email - outlook.

--- resources/views/emails/experiencias/_info-inscricao-candidato-dados-experiencia.blade.php
+++ resources/views/emails/experiencias/_info-inscricao-candidato-dados-experiencia.blade.php
@@ -1,10 +1,11 @@
-<td bgcolor="#FFFFFF" style="clear:both!important; display:block!important; margin:0 auto!important; max-width:600px!important; padding:0px 30px 0 30px;">
+<td bgcolor="#FFFFFF" style="clear:both!important; display:block!important; margin:0 auto!important; max-width:600px!important; padding:10px 30px 0 30px;">
   <div style="display:block; margin:0 auto; max-width:600px;">
     <table style="width: 100%; padding-bottom:0;">
       <tbody>
+      
         <!-- Seção RESUMO DA EXPERIÊNCIA -->
-        <tr>
-          <td>
+        <tr align="left" style="text-align:left;">
+          <td align="left" style="text-align:left;">
             <h3 style="font-family:'FuturaBT Bold', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:20px; font-weight:bolder; color:#545454; line-height:1.2em; margin-top:0; margin-bottom:10px;">
               Resumo da Experiência
             </h3>
@@ -39,52 +40,90 @@
         </tr>
         <!-- Fim da Seção RESUMO DA EXPERIÊNCIA -->
 
-        <!-- Seção DESCRIÇÃO DA EXPERIÊNCIA -->
-        <tr>
-          <td style="padding-bottom:10px;">
-            <h3 style="font-family:'FuturaBT Bold', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:20px; font-weight:bolder; color:#545454; line-height:1.2em; margin-top:0; margin-bottom:10px;">
-              Detalhes
-            </h3>
-            <p style="text-align:justify; font-family:'Avenir Roman', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:16px; font-weight:200; color:#545454; line-height:1.2em; margin-top:0;">
-              {{ trim($Inscricao->experiencia->detalhes) }}
-            </p>
+        <!-- Seção DETALHES DA EXPERIÊNCIA -->
+        <tr style="padding-bottom:10px;">
+          <td>
+            <table>
+              <tbody>
+                <tr>
+                  <td align="left" style="text-align:left;">
+                    <h3 style="font-family:'FuturaBT Bold', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:20px; font-weight:bolder; color:#545454; line-height:1.2em; margin-top:0; margin-bottom:10px;">
+                      Detalhes
+                    </h3>
+                  </td>
+                </tr>
+                <tr>
+                  <td align="left" style="text-align:left;">
+                    <p style="text-align:justify; font-family:'Avenir Roman', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:16px; font-weight:200; color:#545454; line-height:1.2em; margin-top:0;">
+                      {{ trim($Inscricao->experiencia->detalhes) }}
+                    </p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </td>
         </tr>
-        <!-- Fim da Seção DESCRIÇÃO DA EXPERIÊNCIA -->
+        
+        <!-- Fim da Seção DETALHES DA EXPERIÊNCIA -->
 
-        <!-- Seção de INFORMAÇÃO DA EXPERIÊNCIA -->
-        <tr>
+        <!-- Seção de INFORMAÇÕES EXTRAS DA EXPERIÊNCIA -->
+        <tr style="padding-bottom:0px;">
           <td>
-            <h3 style="font-family:'FuturaBT Bold', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:20px; font-weight:bolder; color:#545454; line-height:1.2em; margin-top:0; margin-bottom:10px;">
-              Informações Extras
-            </h3>
+            <table>
+              <tbody>
+                <tr>
+                  <td align="left" style="text-align:left;">
+                    <h3 style="font-family:'FuturaBT Bold', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:20px; font-weight:bolder; color:#545454; line-height:1.2em; margin-top:0; margin-bottom:10px;">
+                      Informações Extras
+                    </h3>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <table>
+                      <tbody>
+                        <tr valign="middle" style="vertical-align:middle;">
+                          <td>
+                            <img src="{{ asset('img/icones/png/cinza-calendario.png') }}" min-width="24px" width="24px" max-width="24px" min-height="24px" height="24px" max-height="24px" style="margin-right:20px;"/>
+                          </td>
+                          <td>
+                            <p style="font-family:'Avenir Roman', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:16px; font-weight:200; color:#545454; line-height:1em;">
+                              {{ ucfirst(mb_strtolower(trim($Inscricao->experiencia->frequencia), 'utf-8')) }}
+                            </p>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>      
+                  </td>
+                </tr>
+                @if($Inscricao->experiencia->informacoes)
+                <tr>
+                  <td>
+                    <table>
+                      <tbody>
+                        @foreach($Inscricao->experiencia->informacoes as $Informacao)
+                        <tr valign="middle" style="vertical-align:middle;">
+                          <td>
+                            <img src="{{ $Informacao->PathIconePNG }}" min-width="24px" width="24px" max-width="24px" min-height="24px" height="24px" max-height="24px" style="margin-right:20px;"/>
+                          </td>
+                          <td>
+                            <p style="font-family:'Avenir Roman', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:16px; font-weight:200; color:#545454; line-height:1em;">
+                              {{ ucfirst(mb_strtolower(trim($Informacao->descricao), 'utf-8')) }}
+                            </p>
+                          </td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+                @endif
+              </tbody>
+            </table>
           </td>
         </tr>
-        <tr>
-          <td>
-            <p style="float:left; margin-top:0px; margin-right:20px; margin-bottom:0px;">
-              <img src="{{ asset('img/icones/png/cinza-calendario.png') }}" min-width="24px" width="24px" max-width="24px" min-height="24px" height="24px" max-height="24px"/>
-            </p>
-            <p style="font-family:'Avenir Roman', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:16px; font-weight:200; color:#545454; line-height:1em; margin-top:6px; margin-bottom:0px;">
-              {{ ucfirst(mb_strtolower(trim($Inscricao->experiencia->frequencia), 'utf-8')) }}
-            </p>
-          </td>
-        </tr>
-        @if($Inscricao->experiencia->informacoes)
-          @foreach($Inscricao->experiencia->informacoes as $Informacao)
-            <tr>
-              <td>
-                <p style="float:left; margin-top:0px; margin-right:20px; margin-bottom:0px;">
-                  <img src="{{ $Informacao->PathIconePNG }}" min-width="24px" width="24px" max-width="24px" min-height="24px" height="24px" max-height="24px"/>
-                </p>
-                <p style="font-family:'Avenir Roman', 'Trebuchet MS', Helvetica, Arial, sans-serif; font-size:16px; font-weight:200; color:#545454; line-height:1em; margin-top:6px; margin-bottom: 5px;">
-                  {{ ucfirst(mb_strtolower(trim($Informacao->descricao), 'utf-8')) }}
-                </p>
-              </td>
-            </tr>
-          @endforeach
-        @endif
-        <!-- Fim da Seção de INFORMAÇÃO DA EXPERIÊNCIA -->
+        <!-- Fim da Seção de INFORMAÇÕES EXTRAS DA EXPERIÊNCIA -->
+
       </tbody>
     </table>
   </div>
